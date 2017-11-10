d18ce84c5b707741ff2fe44d22d8dee8d76fd7e7
Yusuke Hirao <yusukehirao@me.com>
2017-11-10 20:13:57 +0900

    WIP

--- src/rules/markuplint-rule-verify-permitted-contents.test.js
+++ src/rules/markuplint-rule-verify-permitted-contents.test.js
@@ -4,23 +4,43 @@ import rule from '../../lib/rules/markuplint-rule-verify-permitted-contents';
 
 const ruleset = require('../../rulesets/html-ls.json');
 
-// test('HTML Living Standard empty document', async t => {
-// 	const r = await markuplint.verify(``, ruleset, [rule]);
-// 	t.deepEqual(r, [
-// 		'<#doctype> is required that is premitted content of <#root>.',
-// 		'<html> is required that is premitted content of <#root>.',
-// 	]);
-// });
+test('HTML Living Standard empty document', async t => {
+	const r = await markuplint.verify(``, ruleset, [rule]);
+	t.deepEqual(r, [
+		{
+			message: '<#doctype> is required that is premitted content of <#root>.',
+			line: 0,
+			col: 0,
+			raw: '',
+		},
+		{
+			message: '<html> is required that is premitted content of <#root>.',
+			line: 0,
+			col: 0,
+			raw: '',
+		},
+	]);
+});
 
-// test('HTML Living Standard empty html', async t => {
-// 	const r = await markuplint.verify(`
-// 		<!DOCTYPE html>
-// 		<html></html>
-// 	`, ruleset, [rule]);
-// 	t.deepEqual(r, [
-// 		'<head> is required that is premitted content of <html>.',
-// 		'<body> is required that is premitted content of <html>.',
-// 	]);
-// });
+test('HTML Living Standard empty html', async t => {
+	const r = await markuplint.verify(`
+		<!DOCTYPE html>
+		<html></html>
+	`, ruleset, [rule]);
+	t.deepEqual(r, [
+		{
+			message: '<head> is required that is premitted content of <html>.',
+			line: 0,
+			col: 0,
+			raw: '',
+		},
+		{
+			message: '<body> is required that is premitted content of <html>.',
+			line: 0,
+			col: 0,
+			raw: '',
+		},
+	]);
+});
 
 test('noop', t => t.pass());
