84fd36a583008fe62a350157a70c347680dca918
Yusuke Hirao <yusukehirao@me.com>
2018-01-02 20:40:44 +0900

    [WIP] void-element-closing

--- src/rules/markuplint-rule-void-element-closing/README.ja.md
+++ src/rules/markuplint-rule-void-element-closing/README.ja.md
@@ -1,3 +1,1 @@
 # markuplint-rule-void-element-closing
-
-WIP
--- src/rules/markuplint-rule-void-element-closing/index.ts
+++ src/rules/markuplint-rule-void-element-closing/index.ts
@@ -3,19 +3,37 @@ import {
 } from '../../parser';
 import Rule, {
 	RuleConfig,
-	VerifiedResult,
+	VerifyReturn,
 } from '../../rule';
 import Ruleset from '../../ruleset';
+import messages from '../messages';
 
-export type DefaultValue = string;
+export type DefaultValue = null;
 export interface Options {}
 
 export default class extends Rule<DefaultValue, Options> {
 	public name = 'void-element-closing';
+	public defaultLevel: 'error' | 'warning' = 'warning';
 
-	public async verify (document: Document, config: RuleConfig<DefaultValue, Options>, ruleset: Ruleset) {
-		const reports: VerifiedResult[] = [];
-		// document.walk((node) => {});
+	public async verify (document: Document, config: RuleConfig<DefaultValue, Options>, ruleset: Ruleset, locale: string) {
+		const reports: VerifyReturn[] = [];
+		const message = await messages(locale, `空要素に閉じスラッシュがあります`);
+		await document.walkOn('Element', async (node) => {
+			// console.log(node);
+			// console.log(node.nodeName, node.endTagNode, node.endTagLocation);
+			const hasEndTag = !!node.endTagLocation;
+			if (!hasEndTag) {
+				if (/\/>$/.test(node.raw)) {
+					reports.push({
+						level: this.defaultLevel,
+						message,
+						line: node.line,
+						col: node.col,
+						raw: node.raw,
+					});
+				}
+			}
+		});
 		return reports;
 	}
 }
