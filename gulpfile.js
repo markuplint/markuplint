6453f71c7a94153457e5123baff51d75eee66fd2
Yusuke Hirao <yusukehirao@me.com>
2018-01-15 22:41:09 +0900

    remove gulp-ava

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,10 @@ const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const tsc = require('typescript');
 const del = require('del');
-const ava = require('gulp-ava');
-const plumber = require('gulp-plumber');
 
 const project = ts.createProject('./tsconfig.json', {typescript: tsc});
 
 gulp.task('ts', () => project.src().pipe(project()).pipe(gulp.dest('./lib/')));
-
-gulp.task('test', () => gulp.src('src/**/*.test.js').pipe(plumber()).pipe(ava()));
 
 gulp.task('json', () => gulp.src('src/rules/**/*.json').pipe(gulp.dest('lib/rules/')));
 
@@ -19,9 +15,8 @@ gulp.task('clean', () => del(['lib/**/*']));
 
 gulp.task('watch', () => {
 	gulp.watch('src/**/*.ts', gulp.parallel('ts', 'json'));
-	gulp.watch(['src/**/*.test.js', 'lib/**/*.js'], gulp.parallel('test'));
 	gulp.watch(['src/rules/**/*.json'], gulp.parallel('ts', 'json'));
 });
 
-gulp.task('build', gulp.series('clean', gulp.parallel('ts', 'docs', 'json'), 'test'));
+gulp.task('build', gulp.series('clean', gulp.parallel('ts', 'docs', 'json')));
 
