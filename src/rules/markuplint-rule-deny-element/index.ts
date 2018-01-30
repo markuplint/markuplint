61e1f01ddcf7fa7517c56dedc0524cad143e2806
Yusuke Hirao <yusukehirao@me.com>
2018-01-31 01:16:39 +0900

    ✅ deny-element

---
+++ src/rules/markuplint-rule-deny-element/index.ts
@@ -0,0 +1,29 @@
+import {
+	CustomRule,
+	VerifyReturn,
+} from '../../rule';
+import messages from '../messages';
+
+export default CustomRule.create({
+	name: 'deny-element',
+	defaultLevel: 'warning',
+	defaultValue: null,
+	defaultOptions: null,
+	async verify (document, locale) {
+		const reports: VerifyReturn[] = [];
+		await document.walkOn('Element', async (node) => {
+			if (!node.rule) {
+				return;
+			}
+			const message = await messages(locale, '{0} は許可されていません');
+			reports.push({
+				level: node.rule.level,
+				message,
+				line: node.line,
+				col: node.col,
+				raw: node.raw,
+			});
+		});
+		return reports;
+	},
+});
