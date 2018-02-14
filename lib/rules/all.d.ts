86b7a3c95b26bd1bdf3e17abb765b589ebb128ff
Yusuke Hirao <yusukehirao@me.com>
2018-02-14 20:44:44 +0900

    new build

--- lib/rules/all.d.ts
+++ lib/rules/all.d.ts
@@ -1,2 +1,4 @@
-declare const _default: any[];
+import CustomRule from '../rule/custom-rule';
+import { Options } from './markuplint-rule-required-h1';
+declare const _default: (CustomRule<"always" | "never", null> | CustomRule<null, null> | CustomRule<"always" | "never" | "always-single-line" | "never-single-line", null> | CustomRule<"double" | "single", null> | CustomRule<"no-upper" | "no-lower", null> | CustomRule<"lower" | "upper", null> | CustomRule<boolean, null> | CustomRule<string | string[] | null, null> | CustomRule<number | "tab", null> | CustomRule<boolean, Options>)[];
 export default _default;
