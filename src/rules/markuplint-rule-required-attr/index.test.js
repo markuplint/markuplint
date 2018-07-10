8bbcf31689ba02c8a8651d17db1ec7a15f63a851
ktsn <ktsn55@gmail.com>
2018-07-10 12:06:50 +0900

    add a test case for multiple required attributes

--- src/rules/markuplint-rule-required-attr/index.test.js
+++ src/rules/markuplint-rule-required-attr/index.test.js
@@ -34,3 +34,57 @@ test('warns if specified attribute is not appeared', async (t) => {
 		},
 	]);
 });
+
+test('multiple required attributes', async (t) => {
+	const r = await markuplint.verify(
+		'<img src="/path/to/image.png">',
+		{
+			rules: {
+				'required-attr': true,
+			},
+			nodeRules: [
+				{
+					tagName: 'img',
+					rules: {
+						'required-attr': [
+							'error',
+							['width', 'height', 'alt'],
+						],
+					},
+				},
+			],
+		},
+		[rule],
+		'en',
+	);
+
+	t.deepEqual(r, [
+		{
+			severity: 'error',
+			level: 'error',
+			message: 'Required \'width\' on \'<img>\'',
+			line: 1,
+			col: 1,
+			raw: '<img src="/path/to/image.png">',
+			ruleId: 'required-attr',
+		},
+		{
+			severity: 'error',
+			level: 'error',
+			message: 'Required \'height\' on \'<img>\'',
+			line: 1,
+			col: 1,
+			raw: '<img src="/path/to/image.png">',
+			ruleId: 'required-attr',
+		},
+		{
+			severity: 'error',
+			level: 'error',
+			message: 'Required \'alt\' on \'<img>\'',
+			line: 1,
+			col: 1,
+			raw: '<img src="/path/to/image.png">',
+			ruleId: 'required-attr',
+		},
+	]);
+});
