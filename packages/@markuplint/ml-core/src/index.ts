20a402bbf91eb97013b4a471d954bab7310a1b22
Yusuke Hirao <yusukehirao@me.com>
2018-07-16 23:42:45 +0900

    WIP change core API

--- packages/@markuplint/ml-core/src/index.ts
+++ packages/@markuplint/ml-core/src/index.ts
@@ -1,17 +1,17 @@
 import { MLMarkupLanguageParser } from '@markuplint/ml-ast';
 
 import Messenger from './locale/messenger';
-import MLDOM from './ml-dom';
+import MLDOMDocument from './ml-dom/document';
 import Ruleset from './ruleset/core';
 
 export default class Markuplint {
-	public document: MLDOM.Document;
+	public document: MLDOMDocument;
 	public ruleset: Ruleset;
 	public messenger: Messenger;
 
 	constructor(parser: MLMarkupLanguageParser, sourceCode: string, ruleset: Ruleset, messenger: Messenger) {
 		const ast = parser(sourceCode);
-		this.document = MLDOM.generate(ast, ruleset);
+		this.document = new MLDOMDocument(ast, ruleset);
 		this.ruleset = ruleset;
 		this.messenger = messenger;
 	}
