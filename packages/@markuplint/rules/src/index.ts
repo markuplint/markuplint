dc3c421d10465f612c87e7b621465d47c18a07a1
Yusuke Hirao <yusukehirao@me.com>
2018-10-31 01:25:32 +0900

    set rules

--- packages/@markuplint/rules/src/index.ts
+++ packages/@markuplint/rules/src/index.ts
@@ -1,3 +1,91 @@
-import asyncAttrInScript from './async-attr-in-script';
+import AsyncAttrInScript from './async-attr-in-script';
+import AttrDuplication from './attr-duplication';
+import AttrEqualSpaceAfter from './attr-equal-space-after';
+import AttrEqualSpaceBefore from './attr-equal-space-before';
+import AttrSpasing from './attr-spasing';
+import AttrValueQuotes from './attr-value-quotes';
+import CaseSensitiveAttrName from './case-sensitive-attr-name';
+import CaseSensitiveTagName from './case-sensitive-tag-name';
+import CharacterReference from './character-reference';
+import ClassNaming from './class-naming';
+import Comment from './comment';
+import CommentSpasing from './comment-spasing';
+import CustomElementNaming from './custom-element-naming';
+import DataAttrNaming from './data-attr-naming';
+import DenyElement from './deny-element';
+import DeprecatedAriaAttr from './deprecated-aria-attr';
+import DeprecatedAttr from './deprecated-attr';
+import DeprecatedElement from './deprecated-element';
+import DeprecatedGlobalAttr from './deprecated-global-attr';
+import Doctype from './doctype';
+import EmptyAltAttr from './empty-alt-attr';
+import EventAttr from './event-attr';
+import ExternalLink from './external-link';
+import HeadingInSectioningContent from './heading-in-sectioning-content';
+import HeadingInSectioningRoot from './heading-in-sectioning-root';
+import HeadingLevelsSkipping from './heading-levels-skipping';
+import IdDuplication from './id-duplication';
+import Indentation from './indentation';
+import IndentationAttr from './indentation-attr';
+import Labels from './labels';
+import LandmarkRoles from './landmark-roles';
+import MultilineAttr from './multiline-attr';
+import MultilineTag from './multiline-tag';
+import OmittedClosingTag from './omitted-closing-tag';
+import ParseError from './parse-error';
+import Path from './path';
+import PermittedContents from './permitted-contents';
+import PermittedRole from './permitted-role';
+import RequiredAttr from './required-attr';
+import RequiredElement from './required-element';
+import RequiredH1 from './required-h1';
+import RoleStructureTab from './role-structure-tab';
+import SelfClosingTag from './self-closing-tag';
+import VoidElementClosing from './void-element-closing';
 
-export default [asyncAttrInScript];
+export default [
+	AsyncAttrInScript,
+	AttrDuplication,
+	AttrEqualSpaceAfter,
+	AttrEqualSpaceBefore,
+	AttrSpasing,
+	AttrValueQuotes,
+	CaseSensitiveAttrName,
+	CaseSensitiveTagName,
+	CharacterReference,
+	ClassNaming,
+	Comment,
+	CommentSpasing,
+	CustomElementNaming,
+	DataAttrNaming,
+	DenyElement,
+	DeprecatedAriaAttr,
+	DeprecatedAttr,
+	DeprecatedElement,
+	DeprecatedGlobalAttr,
+	Doctype,
+	EmptyAltAttr,
+	EventAttr,
+	ExternalLink,
+	HeadingInSectioningContent,
+	HeadingInSectioningRoot,
+	HeadingLevelsSkipping,
+	IdDuplication,
+	Indentation,
+	IndentationAttr,
+	Labels,
+	LandmarkRoles,
+	MultilineAttr,
+	MultilineTag,
+	OmittedClosingTag,
+	ParseError,
+	Path,
+	PermittedContents,
+	PermittedRole,
+	RequiredAttr,
+	RequiredElement,
+	RequiredH1,
+	RoleStructureTab,
+	SelfClosingTag,
+	VoidElementClosing,
+];
