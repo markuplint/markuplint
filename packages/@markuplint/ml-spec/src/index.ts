701013b4c1fa31208bedb79fe285cd7a9dc1db64
Yusuke Hirao <yusukehirao@me.com>
2019-03-29 01:10:32 +0900

    Optimize ml-spec

--- packages/@markuplint/ml-spec/src/index.ts
+++ packages/@markuplint/ml-spec/src/index.ts
@@ -1,1 +1,34 @@
-export { MLMLSpec } from './types';
+import { MLMLSpec, ElementSpec, ElementCategories } from './types';
+export * from './types';
+
+export interface SpecOM {
+	[tagName: string]: MLDOMElementSpec;
+}
+
+export interface MLDOMElementSpec {
+	experimental: boolean;
+	obsolete: boolean | string;
+	deprecated: boolean;
+	nonStandard: boolean;
+	categories: ElementCategories;
+}
+
+export function getSpecOM({ specs }: MLMLSpec): SpecOM {
+	const som: SpecOM = {};
+	for (const el of specs) {
+		som[el.name] = {
+			experimental: !!el.experimental,
+			obsolete: typeof el.obsolete === 'boolean' ? !!el.obsolete : el.obsolete ? el.obsolete.alt : false,
+			deprecated: !!el.deprecated,
+			nonStandard: !!el.nonStandard,
+			categories: el.categories,
+		};
+	}
+	return som;
+}
+
+export function getSpecByTagName(tagName: string, specOM: SpecOM): MLDOMElementSpec {
+	tagName = tagName.toLowerCase();
+	const spec = specOM[tagName];
+	return spec || null;
+}
