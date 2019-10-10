bdbc09ce60493ca0e7d871c93270330fe8aea9b7
Yusuke Hirao <yusukehirao@me.com>
2019-10-10 21:52:15 +0900

    Add permitted-contents rule

---
+++ packages/@markuplint/rules/src/permitted-contents/index.spec.ts
@@ -0,0 +1,275 @@
+import * as markuplint from 'markuplint';
+import rule from './';
+
+describe('verify', () => {
+	test('ui > li', async () => {
+		const o = {
+			rules: {
+				'permitted-contents': {
+					option: [
+						{
+							tag: 'ul',
+							contents: [
+								{
+									oneOrMore: 'li',
+								},
+							],
+						},
+					],
+				},
+			},
+		};
+		const r1 = await markuplint.verify('<ul></ul>', o, [rule], 'en');
+		const r2 = await markuplint.verify('<ul><li>0</li></ul>', o, [rule], 'en');
+		const r3 = await markuplint.verify('<ul><li>0</li><li>1</li><li>2</li></ul>', o, [rule], 'en');
+		const r4 = await markuplint.verify('<ul> <li>0</li> <li>1</li> <li>2</li> </ul>', o, [rule], 'en');
+		const r5 = await markuplint.verify('<ul> <li>0</li> <li>1</li> <li>2</li> 3 </ul>', o, [rule], 'en');
+		expect(r1).toStrictEqual([
+			{
+				ruleId: 'permitted-contents',
+				severity: 'error',
+				line: 1,
+				col: 1,
+				raw: '<ul>',
+				message: 'Invalid content, Require one or more "li" element',
+			},
+		]);
+		expect(r2).toStrictEqual([]);
+		expect(r3).toStrictEqual([]);
+		expect(r4).toStrictEqual([]);
+		expect(r5).toStrictEqual([
+			{
+				col: 38,
+				line: 1,
+				message: 'Invalid contents, "ul" is unpermitted to contain "TextNode"',
+				raw: ' 3 ',
+				ruleId: 'permitted-contents',
+				severity: 'error',
+			},
+		]);
+	});
+
+	test('table > tbody', async () => {
+		const o = {
+			rules: {
+				'permitted-contents': {
+					option: [
+						{
+							tag: 'table',
+							contents: [
+								{
+									require: 'tbody',
+								},
+							],
+						},
+					],
+				},
+			},
+		};
+		const r1 = await markuplint.verify('<table></table>', o, [rule], 'en');
+		const r2 = await markuplint.verify('<table><tbody></tbody></table>', o, [rule], 'en');
+		const r3 = await markuplint.verify('<table><tbody></tbody><tbody></tbody></table>', o, [rule], 'en');
+		expect(r1).toStrictEqual([
+			{
+				ruleId: 'permitted-contents',
+				severity: 'error',
+				line: 1,
+				col: 1,
+				raw: '<table>',
+				message: 'Invalid content, Require "tbody" element',
+			},
+		]);
+		expect(r2).toStrictEqual([]);
+		expect(r3).toStrictEqual([
+			{
+				ruleId: 'permitted-contents',
+				severity: 'error',
+				line: 1,
+				col: 1,
+				raw: '<table>',
+				message: 'Invalid content, The maximum length of "tbody" elements is 1',
+			},
+		]);
+	});
+
+	test('The strict table', async () => {
+		const o = {
+			rules: {
+				'permitted-contents': {
+					option: [
+						{
+							tag: 'table',
+							contents: [
+								{
+									require: 'caption',
+								},
+								{
+									optional: 'thead',
+								},
+								{
+									optional: 'tfoot',
+								},
+								{
+									require: 'tbody',
+								},
+							],
+						},
+					],
+				},
+			},
+		};
+		const r1 = await markuplint.verify('<table></table>', o, [rule], 'en');
+		const r2 = await markuplint.verify('<table><caption></caption><tbody></tbody></table>', o, [rule], 'en');
+		const r3 = await markuplint.verify(
+			'<table><caption></caption><thead></thead><tbody></tbody></table>',
+			o,
+			[rule],
+			'en',
+		);
+		const r4 = await markuplint.verify(
+			'<table><caption></caption><tfoot></tfoot><tbody></tbody></table>',
+			o,
+			[rule],
+			'en',
+		);
+		const r5 = await markuplint.verify(
+			'<table><caption></caption><thead></thead><tfoot></tfoot><tbody></tbody></table>',
+			o,
+			[rule],
+			'en',
+		);
+		const r6 = await markuplint.verify(
+			'<table><caption></caption><thead></thead><tfoot></tfoot><tr></tr><tr></tr><tr></tr></table>',
+			o,
+			[rule],
+			'en',
+		);
+		expect(r1).toStrictEqual([
+			{
+				ruleId: 'permitted-contents',
+				severity: 'error',
+				line: 1,
+				col: 1,
+				raw: '<table>',
+				message: 'Invalid content, Require "caption" element',
+			},
+			{
+				ruleId: 'permitted-contents',
+				severity: 'error',
+				line: 1,
+				col: 1,
+				raw: '<table>',
+				message: 'Invalid content, Require "tbody" element',
+			},
+		]);
+		expect(r2).toStrictEqual([]);
+		expect(r3).toStrictEqual([]);
+		expect(r4).toStrictEqual([]);
+		expect(r5).toStrictEqual([]);
+		expect(r6).toStrictEqual([
+			{
+				ruleId: 'permitted-contents',
+				severity: 'error',
+				line: 1,
+				col: 1,
+				raw: '<table>',
+				message: 'Invalid content, Require "tbody" element',
+			},
+			{
+				ruleId: 'permitted-contents',
+				severity: 'error',
+				line: 1,
+				col: 57,
+				raw: '<tr>',
+				message: 'Invalid contents, "table" is unpermitted to contain "tr"',
+			},
+		]);
+	});
+
+	test('Custom element', async () => {
+		const o = {
+			rules: {
+				'permitted-contents': {
+					option: [
+						{
+							tag: 'x-container',
+							contents: [
+								{
+									require: 'x-item',
+									min: 2,
+									max: 5,
+								},
+							],
+						},
+					],
+				},
+			},
+		};
+		const r1 = await markuplint.verify('<x-container></x-container>', o, [rule], 'en');
+		const r2 = await markuplint.verify('<x-container><x-item>0</x-item></x-container>', o, [rule], 'en');
+		const r3 = await markuplint.verify(
+			'<x-container><x-item>0</x-item><x-item>1</x-item><x-item>2</x-item></x-container>',
+			o,
+			[rule],
+			'en',
+		);
+		const r4 = await markuplint.verify(
+			`<x-container>
+				<x-item>0</x-item>
+				<x-item>1</x-item>
+				<x-item>2</x-item>
+				<x-item>3</x-item>
+				<x-item>4</x-item>
+			</x-container>`,
+			o,
+			[rule],
+			'en',
+		);
+		const r5 = await markuplint.verify(
+			`<x-container>
+				<x-item>0</x-item>
+				<x-item>1</x-item>
+				<x-item>2</x-item>
+				<x-item>3</x-item>
+				<x-item>4</x-item>
+				<x-item>5</x-item>
+				<x-item>6</x-item>
+			</x-container>`,
+			o,
+			[rule],
+			'en',
+		);
+		expect(r1).toStrictEqual([
+			{
+				ruleId: 'permitted-contents',
+				severity: 'error',
+				line: 1,
+				col: 1,
+				raw: '<x-container>',
+				message: 'Invalid content, Require "x-item" element at least 2',
+			},
+		]);
+		expect(r2).toStrictEqual([
+			{
+				ruleId: 'permitted-contents',
+				severity: 'error',
+				line: 1,
+				col: 1,
+				raw: '<x-container>',
+				message: 'Invalid content, Require "x-item" element at least 2',
+			},
+		]);
+		expect(r3).toStrictEqual([]);
+		expect(r4).toStrictEqual([]);
+		expect(r5).toStrictEqual([
+			{
+				ruleId: 'permitted-contents',
+				severity: 'error',
+				line: 1,
+				col: 1,
+				raw: '<x-container>',
+				message: 'Invalid content, The maximum length of "x-item" elements is 5',
+			},
+		]);
+	});
+});
--- packages/@markuplint/rules/src/permitted-contents/index.ts
+++ packages/@markuplint/rules/src/permitted-contents/index.ts
@@ -1,25 +1,165 @@
 import { Result, createRule } from '@markuplint/ml-core';
 
-export default createRule({
-	name: 'name',
-	defaultValue: null,
-	defaultOptions: null,
+type TagRule = {
+	tag: string;
+	contents: Contents[];
+};
+
+type Contents = Required | Optional | OneOrMore | ZeroOrMore | Choice;
+
+type Required = {
+	require: string;
+	min?: number;
+	max?: number;
+};
+
+type Optional = {
+	optional: string;
+	max?: number;
+};
+
+type OneOrMore = {
+	oneOrMore: string;
+	max?: number;
+};
+
+type ZeroOrMore = {
+	zeroOrMore: string;
+	max?: number;
+};
+
+type Choice = {
+	choice: Contents[];
+};
+
+export default createRule<boolean, TagRule[]>({
+	name: 'permitted-contents',
+	defaultValue: true,
+	defaultOptions: [],
 	async verify(document, messages) {
 		const reports: Result[] = [];
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		const message = messages('error');
 		await document.walkOn('Element', async node => {
-			// eslint-disable-next-line no-constant-condition
-			if (true) {
-				// reports.push({
-				// 	level: node.rule.level,
-				// 	message,
-				// 	line: node.line,
-				// 	col: node.col,
-				// 	raw: node.raw,
-				// });
+			if (!node.rule.value) {
+				return;
+			}
+			// console.log(node.rule.option);
+			for (const rule of node.rule.option) {
+				if (rule.tag.toLowerCase() !== node.nodeName.toLowerCase()) {
+					continue;
+				}
+
+				const childElementsAndTextOrigin = Object.freeze(node.getChildElementsAndTextNodeWithoutWhitespaces());
+
+				/**
+				 * @mutable
+				 */
+				const childElementsAndText = [...childElementsAndTextOrigin];
+
+				for (const ruleContent of rule.contents) {
+					let permittedTag: string | null = null;
+					let min = 0;
+					let max = Infinity;
+					let numPrefix = '';
+					if (isRequiredContents(ruleContent)) {
+						permittedTag = ruleContent.require;
+						min = ruleContent.min || 1;
+						max = ruleContent.max || 1;
+					} else if (isOptionalContents(ruleContent)) {
+						permittedTag = ruleContent.optional;
+						min = 0;
+						max = ruleContent.max || 1;
+					} else if (isOneOrMoreContents(ruleContent)) {
+						permittedTag = ruleContent.oneOrMore;
+						min = 1;
+						max = ruleContent.max || Infinity;
+						numPrefix = 'one or more ';
+					} else if (isZeroOrMoreContents(ruleContent)) {
+						permittedTag = ruleContent.zeroOrMore;
+						min = 0;
+						max = ruleContent.max || Infinity;
+					}
+
+					if (!permittedTag) {
+						continue;
+					}
+
+					const childElementsAndTextNameList = childElementsAndText.map(node =>
+						node.type === 'Element' ? node.nodeName.toLowerCase() : '#text',
+					);
+
+					let count = 0;
+					for (const childElemName of childElementsAndTextNameList) {
+						if (childElemName === permittedTag) {
+							count++;
+							continue;
+						}
+						break;
+					}
+					// console.log({ childElementsAndTextNameList, count });
+
+					if (count < min) {
+						reports.push({
+							severity: node.rule.severity,
+							message: messages(
+								`Invalid content, Require ${numPrefix}"${permittedTag}" element${
+									!numPrefix && 1 < min ? ` at least ${min}` : ''
+								}`,
+							),
+							line: node.startLine,
+							col: node.startCol,
+							raw: node.raw,
+						});
+					}
+
+					if (count > max) {
+						reports.push({
+							severity: node.rule.severity,
+							message: messages(
+								`Invalid content, The maximum length of "${permittedTag}" elements is ${max}`,
+							),
+							line: node.startLine,
+							col: node.startCol,
+							raw: node.raw,
+						});
+					}
+
+					childElementsAndText.splice(0, count);
+				}
+
+				if (childElementsAndText[0]) {
+					// console.log(childElementsAndText);
+					const el = childElementsAndText[0];
+					const name = el.type === 'Element' ? el.nodeName : 'TextNode';
+					reports.push({
+						severity: node.rule.severity,
+						message: messages(`Invalid contents, "${node.nodeName}" is unpermitted to contain "${name}"`),
+						line: el.startLine,
+						col: el.startCol,
+						raw: el.raw,
+					});
+				}
 			}
 		});
 		return reports;
 	},
 });
+
+function isRequiredContents(contents: Contents): contents is Required {
+	return 'require' in contents;
+}
+
+function isOptionalContents(contents: Contents): contents is Optional {
+	return 'optional' in contents;
+}
+
+function isOneOrMoreContents(contents: Contents): contents is OneOrMore {
+	return 'oneOrMore' in contents;
+}
+
+function isZeroOrMoreContents(contents: Contents): contents is ZeroOrMore {
+	return 'zeroOrMore' in contents;
+}
+
+// function isChoiceContents(contents: Contents): contents is Choice {
+// 	return 'choice' in contents;
+// }
