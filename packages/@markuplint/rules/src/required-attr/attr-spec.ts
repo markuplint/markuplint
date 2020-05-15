b44b142d777945ee1a9c1fa9c6ee860ba3ca8cb3
Yusuke Hirao <yusukehirao@me.com>
2020-05-15 18:58:52 +0900

    Update rule "required-attr" on HTML LS specs

---
+++ packages/@markuplint/rules/src/required-attr/attr-spec.ts
@@ -0,0 +1,35 @@
+import html, { Attribute } from '@markuplint/html-ls';
+
+export default function attrSpec(tag: string) {
+	tag = tag.toLowerCase();
+	const spec = html.specs.find(spec => spec.name === tag);
+	if (!spec) {
+		return [];
+	}
+	const hasGlobalAttr = spec.attributes.some(attr => attr === '#globalAttrs');
+
+	const attrs: Attribute[] = [];
+
+	if (hasGlobalAttr) {
+		attrs.push(...html.def['#globalAttrs']);
+	}
+
+	for (const attr of spec.attributes) {
+		if (typeof attr === 'string') {
+			continue;
+		}
+
+		const definedIndex = attrs.findIndex(a => a.name === attr.name);
+		if (definedIndex !== -1) {
+			attrs[definedIndex] = {
+				...attrs[definedIndex],
+				...attr,
+			};
+			continue;
+		}
+
+		attrs.push(attr);
+	}
+
+	return attrs;
+}
--- packages/@markuplint/rules/src/required-attr/index.ts
+++ packages/@markuplint/rules/src/required-attr/index.ts
@@ -1,19 +1,49 @@
 import { Result, createRule } from '@markuplint/ml-core';
+import attrSpec from './attr-spec';
 
 type RequiredAttributes = string | string[];
 
 export default createRule<RequiredAttributes, null>({
 	name: 'required-attr',
-	defaultLevel: 'warning',
+	defaultLevel: 'error',
 	defaultValue: [],
 	defaultOptions: null,
 	async verify(document, translate) {
 		const reports: Result[] = [];
 		await document.walkOn('Element', async node => {
-			const requiredAttrs = typeof node.rule.value === 'string' ? [node.rule.value] : node.rule.value;
-			requiredAttrs.forEach(attr => {
-				if (!node.hasAttribute(attr)) {
-					const message = translate('Required {0} on {1}', `'${attr}'`, `'<${node.nodeName}>'`);
+			const customRequiredAttrs = typeof node.rule.value === 'string' ? [node.rule.value] : node.rule.value;
+
+			const attributeSpecs = attrSpec(node.nodeName).map(attr => {
+				const required = customRequiredAttrs.includes(attr.name);
+				if (required) {
+					return {
+						...attr,
+						required: true,
+					};
+				}
+				return attr;
+			});
+
+			for (const spec of attributeSpecs) {
+				let invalid = false;
+				if (spec.requiredEither) {
+					const candidate = [...spec.requiredEither, spec.name];
+					invalid = !candidate.some(attrName => node.hasAttribute(attrName));
+				} else if (spec.required) {
+					if (spec.condition) {
+						if (spec.condition.self) {
+							const condSelector = Array.isArray(spec.condition.self)
+								? spec.condition.self.join(',')
+								: spec.condition.self;
+							invalid = node.matches(condSelector) && !node.hasAttribute(spec.name);
+						}
+					} else {
+						invalid = !node.hasAttribute(spec.name);
+					}
+				}
+
+				if (invalid) {
+					const message = translate('Required {0} on {1}', `'${spec.name}'`, `'<${node.nodeName}>'`);
 					reports.push({
 						severity: node.rule.severity,
 						message,
@@ -22,7 +52,7 @@ export default createRule<RequiredAttributes, null>({
 						raw: node.raw,
 					});
 				}
-			});
+			}
 		});
 
 		return reports;
