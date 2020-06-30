62acaa7ebbf4be21ae8e82f9b278caf9a44a2887
Yusuke Hirao <yusukehirao@me.com>
2020-06-30 19:28:09 +0900

    Export API for AST

--- packages/@markuplint/html-parser/src/index.ts
+++ packages/@markuplint/html-parser/src/index.ts
@@ -2,3 +2,6 @@ export { default as isDocumentFragment } from './is-document-fragment';
 export { default as nodeListToDebugMaps } from './node-list-to-debug-maps';
 export { default as parse } from './parse';
 export { flattenNodes } from './flatten-nodes';
+export { default as getEndCol } from './get-end-col';
+export { default as getEndLine } from './get-end-line';
+export { walk, Walker } from './walk';
