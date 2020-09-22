dc80632952a170fc69bc648ce2d89637f1b18a10
yusukehirao <yusukehirao@me.com>
2020-09-23 01:35:58 +0900

    Fixed OG data.

--- website/src/components/meta/BasicMeta.tsx
+++ website/src/components/meta/BasicMeta.tsx
@@ -13,12 +13,26 @@ export default function BasicMeta({ title, description }: Props) {
 			<title>{title ? [title, siteTitle].join(' | ') : siteTitle}</title>
 			<meta name="description" content={description ? description : ''} />
 			<meta name="viewport" content="width=device-width, initial-scale=1" />
-			<link rel="apple-touch-icon" href="/icon.png" />
 			<meta name="theme-color" content="#fff" />
-			<meta name="twitter:card" content="summary" />
-			<meta name="twitter:site" content="@markuplint" />
+			<meta property="og:type" content="website" />
+			<meta property="og:title" content={title} />
+			<meta
+				property="og:description"
+				content={description ? description : 'Peace of mind in your markup. A Linter for All Markup Languages.'}
+			/>
 			<meta
 				property="og:image"
+				content="https://repository-images.githubusercontent.com/104835801/27437480-da03-11e9-8504-c8407b7d9a13"
+			/>
+			<meta name="twitter:card" content="summary" />
+			<meta name="twitter:site" content="@markuplint" />
+			<meta name="twitter:title" content={title} />
+			<meta
+				name="twitter:description"
+				content={description ? description : 'Peace of mind in your markup. A Linter for All Markup Languages.'}
+			/>
+			<meta
+				name="twitter:image"
 				content="https://repository-images.githubusercontent.com/104835801/27437480-da03-11e9-8504-c8407b7d9a13"
 			/>
 		</Head>
