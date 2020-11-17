7c30797d695aca1eab9b9a74fb06be7c93c111e9
yusukehirao <yusukehirao@me.com>
2020-11-17 19:44:14 +0900

    Improve referring and fetching to the spec of WAI-ARIA.

--- packages/@markuplint/html-spec/gen/aria.ts
+++ packages/@markuplint/html-spec/gen/aria.ts
@@ -1,42 +1,55 @@
 import { ARIAAttribute, ARIAAttributeValue, ARIRRoleAttribute } from '@markuplint/ml-spec';
-import fetch, { fetchText } from './fetch';
+import fetch from './fetch';
 import { nameCompare } from './utils';
-import xml from 'fast-xml-parser';
 
 export async function getAria() {
-	const uml = await fetchText('https://www.w3.org/WAI/ARIA/schemata/aria-1.uml');
-	const dom = xml.getTraversalObj(uml, {
-		ignoreNameSpace: true,
-		ignoreAttributes: false,
-		attributeNamePrefix: '',
-		parseNodeValue: true,
-		parseAttributeValue: true,
-		trimValues: true,
+	const $ = await fetch('https://www.w3.org/TR/wai-aria-1.2/');
+	const $roleList = $('#role_definitions section.role');
+	const roles: ARIRRoleAttribute[] = [];
+	$roleList.each((_, el) => {
+		const $el = $(el);
+		const name = $el.find('.role-name').attr('title')?.trim() || '';
+		const description = $el
+			.find('.role-description p')
+			.toArray()
+			.map(p => $(p).text().trim().replace(/\s+/g, ' ').replace(/\t+/g, ''))
+			.join('\n\n');
+		const $feaures = $el.find('.role-features tr');
+		const generalization = $feaures
+			.find('.role-parent a')
+			.toArray()
+			.map(a => $(a).text().trim());
+		const isAbstract = $feaures.find('.role-abstract').text().trim().toLowerCase() === 'true' || undefined;
+		const ownedAttribute = Array.from(
+			new Set([
+				...$feaures
+					.find('.role-inherited a')
+					.toArray()
+					.map(a =>
+						$(a)
+							.text()
+							.trim()
+							.replace(/\s*\(state\)\s*/, ''),
+					),
+				...$feaures
+					.find('.role-properties a')
+					.toArray()
+					.map(a =>
+						$(a)
+							.text()
+							.trim()
+							.replace(/\s*\(state\)\s*/, ''),
+					),
+			]),
+		).sort();
+		roles.push({
+			name,
+			description,
+			isAbstract,
+			generalization,
+			ownedAttribute,
+		});
 	});
-
-	const $ = await fetch('https://www.w3.org/TR/wai-aria-1.1/');
-	const $indexRole = $('#index_role');
-
-	const roles: ARIRRoleAttribute[] = dom.child.XMI[0].child.Package[0].child.packagedElement.map(
-		(pkgEl: any): ARIRRoleAttribute => {
-			const name = pkgEl.attrsMap.name;
-			const $ref = $indexRole.find(`.role-reference[href="#${name}"]`);
-			const $dt = $ref.closest('dt');
-			const $dd = $dt.next('dd');
-			const description = $dd.text().trim();
-			return {
-				name,
-				description,
-				isAbstract: !!pkgEl.attrsMap.isAbstract || undefined,
-				generalization: pkgEl.child.generalization
-					? pkgEl.child.generalization.map((gen: any) => gen.attrsMap.general)
-					: [],
-				ownedAttribute: pkgEl.child.ownedAttribute
-					? pkgEl.child.ownedAttribute.map((attr: any) => attr.attrsMap.name)
-					: [],
-			};
-		},
-	);
 
 	roles.sort(nameCompare);
 
