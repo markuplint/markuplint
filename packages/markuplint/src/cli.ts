e211d5cb609a02643fe7470e2c1731f98ae241f9
yusukehirao <yusukehirao@me.com>
2021-03-19 20:20:26 +0900

    Improve the CLI input.

--- packages/markuplint/src/cli.ts
+++ packages/markuplint/src/cli.ts
@@ -2,36 +2,30 @@ import { cli } from './cli-bootstrap';
 import { command } from './command';
 import getStdin from 'get-stdin';
 
-if (cli.flags.v) {
-	cli.showVersion(); // And exit successfully.
-}
+(async () => {
+	if (cli.flags.v) {
+		cli.showVersion(); // And exit successfully.
+	}
 
-if (cli.flags.h) {
-	cli.showHelp(0); // And exit successfully.
-}
+	if (cli.flags.h) {
+		cli.showHelp(0); // And exit successfully.
+	}
 
-if (cli.input.length) {
-	command({
-		files: cli.input,
-		...cli.flags,
-	}).catch(err => {
-		process.stderr.write(err + '\n');
-		process.exit(1);
-	});
-} else {
-	cli.showHelp(1); // And fail.
-}
+	const stdin = await getStdin();
+	const files = stdin ? cli.input.concat(stdin) : cli.input;
 
-getStdin().then(stdin => {
-	const html = stdin;
-	if (!html) {
+	if (files.length) {
+		await command({
+			files,
+			...cli.flags,
+		}).catch(err => {
+			process.stderr.write(err + '\n');
+			process.exit(1);
+		});
+	} else {
+		cli.showHelp(1); // And fail.
 		return;
 	}
-	command({
-		codes: html,
-		...cli.flags,
-	}).catch(err => {
-		process.stderr.write(err + '\n');
-		process.exit(1);
-	});
+})().finally(() => {
+	process.exit();
 });
