3eb28ca15895c1740790ccab80f896911776f677
yusukehirao <yusukehirao@me.com>
2021-05-05 14:18:15 +0900

    Move `isPotentialCustomElementName` to parser-utils.

--- packages/@markuplint/parser-utils/src/utils.ts
+++ packages/@markuplint/parser-utils/src/utils.ts
@@ -1,4 +1,5 @@
 import { MLASTAttr, MLASTNode, MLASTNodeType, MLToken, Walker } from '@markuplint/ml-ast';
+import { rePCEN } from './const';
 import { v4 as uuid4 } from 'uuid';
 
 export function uuid() {
@@ -121,6 +122,43 @@ export function siblingsCorrection(nodeList: MLASTNode[]) {
 		node.prevNode = prevNode;
 		node.nextNode = nextNode;
 	}
+}
+
+/**
+ * valid name of custom element
+ *
+ * @see https://html.spec.whatwg.org/multipage/custom-elements.html#valid-custom-element-name
+ *
+ * > - name must match the [PotentialCustomElementName](https://html.spec.whatwg.org/multipage/custom-elements.html#prod-potentialcustomelementname) production
+ * > - name must not be any of the following:
+ * >   - annotation-xml
+ * >   - color-profile
+ * >   - font-face
+ * >   - font-face-src
+ * >   - font-face-uri
+ * >   - font-face-format
+ * >   - font-face-name
+ * >   - missing-glyph
+ *
+ * ASCII-case-insensitively.
+ * Originally, it is not possible to define a name including ASCII upper alphas in the custom element, but it is not treated as illegal by the HTML parser.
+ *
+ * @param tagName
+ */
+export function isPotentialCustomElementName(tagName: string) {
+	switch (tagName) {
+		case 'annotation-xml':
+		case 'color-profile':
+		case 'font-face':
+		case 'font-face-src':
+		case 'font-face-uri':
+		case 'font-face-format':
+		case 'font-face-name':
+		case 'missing-glyph': {
+			return false;
+		}
+	}
+	return rePCEN.test(tagName);
 }
 
 function tokenDebug<
