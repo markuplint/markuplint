8cf919dd29080be01c17ac6564430875aa9220c3
JounQin <admin@1stg.me>
2021-05-14 10:33:04 +0800

    feat: lazy load textlint on Node for browser compatibility

--- packages/@markuplint/rule-textlint/src/index.ts
+++ packages/@markuplint/rule-textlint/src/index.ts
@@ -1,29 +1,34 @@
 import { Result, createRule } from '@markuplint/ml-core';
 import { TextlintKernel, TextlintResult } from '@textlint/kernel';
-import { Config } from 'textlint/lib/config/config';
-import { TextLintEngine } from 'textlint';
 import { TextlintKernelOptions } from '@textlint/kernel/lib/textlint-kernel-interface';
 
 // @ts-ignore / This has no types
 import TextlintPluginHTML from 'textlint-plugin-html';
 
-import path from 'path';
-
-const kernel = new TextlintKernel();
-
 type Option = Partial<TextlintKernelOptions> | true;
+
+const isNode = typeof process !== 'undefined' && !!process.versions.node;
+
+let path: typeof import('path');
+let kernel: TextlintKernel;
+let textlint: typeof import('textlint');
+let _config: typeof import('textlint/lib/config/config');
 
 export default createRule<boolean, Option>({
 	defaultLevel: 'warning',
 	name: 'textlint',
 	defaultValue: true,
-	defaultOptions: true,
+	defaultOptions: isNode || {},
 	async verify(document, translate, config) {
 		const reports: Result[] = [];
 		const html = document.toString();
 		const option = config.option;
 		let textlintResult: TextlintResult;
 		if (typeof option === 'object') {
+			if (!kernel) {
+				kernel = new TextlintKernel();
+			}
+
 			textlintResult = await kernel.lintText(html, {
 				...option,
 				ext: '.html',
@@ -35,23 +40,44 @@ export default createRule<boolean, Option>({
 					},
 				],
 			});
-		} else {
-			if (option === true) {
-				const textlintConfig = Config.initWithAutoLoading({
-					cwd: document.filename && path.dirname(document.filename),
-				});
-				if (!textlintConfig.plugins.includes('html')) {
-					textlintConfig.plugins.push('html');
-					textlintConfig.pluginsConfig.html = true;
-				}
-				const lintEngine = new TextLintEngine(textlintConfig);
-				textlintResult = (await lintEngine.executeOnText(html, '.html'))[0];
-			} else {
+		} else if (option === true) {
+			if (!isNode) {
 				// eslint-disable-next-line no-console
-				console.warn('`config.option` should be `true` or `object`');
+				console.error(
+					'`config.option` with `true` value is only available on Node.js, please use plain `TextlintKernelOptions` instead',
+				);
 				return reports;
 			}
+
+			if (!path) {
+				path = await import('path');
+			}
+
+			if (!textlint) {
+				textlint = await import('textlint');
+			}
+
+			if (!_config) {
+				_config = await import('textlint/lib/config/config');
+			}
+
+			const textlintConfig = _config.Config.initWithAutoLoading({
+				cwd: document.filename && path.dirname(document.filename),
+			});
+
+			if (!textlintConfig.plugins.includes('html')) {
+				textlintConfig.plugins.push('html');
+				textlintConfig.pluginsConfig.html = true;
+			}
+
+			const lintEngine = new textlint.TextLintEngine(textlintConfig);
+			textlintResult = (await lintEngine.executeOnText(html, '.html'))[0];
+		} else {
+			// eslint-disable-next-line no-console
+			console.warn('`config.option` should be `true` or `object`');
+			return reports;
 		}
+
 		for (const result of textlintResult.messages) {
 			const message = translate(`Invalid text: ${result.message}`);
 			const [s, e] = result.fix?.range || [result.index, result.index];
