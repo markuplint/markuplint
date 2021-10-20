da6a0b9a25adcaba6e6c40576eb14d233725727f
yusukehirao <yusukehirao@me.com>
2021-10-20 22:19:31 +0900

    Add test

--- packages/@markuplint/rules/src/invalid-attr/index.spec.ts
+++ packages/@markuplint/rules/src/invalid-attr/index.spec.ts
@@ -472,6 +472,39 @@ test('Foreign element', async () => {
 	expect(violations.length).toBe(0);
 });
 
+test('svg', async () => {
+	expect(
+		(
+			await mlTest(
+				`<svg viewBox="0 0 300 100" xmlns="http://www.w3.org/2000/svg" stroke="red" fill="grey">
+					<circle cx="50" cy="50" cz="50" r="40" />
+					<circle cx="150" cy="50" r="4" />
+					<svg viewBox="0 0 10 10" x="200" width="100">
+						<circle cx="5" cy="5" r="4" />
+					</svg>
+				</svg>
+				`,
+				{
+					rules: {
+						'invalid-attr': true,
+					},
+				},
+				[rule],
+				'en',
+			)
+		).violations,
+	).toStrictEqual([
+		{
+			ruleId: 'invalid-attr',
+			severity: 'error',
+			line: 2,
+			col: 30,
+			message: 'The "cz" attribute is not allowed',
+			raw: 'cz',
+		},
+	]);
+});
+
 test('Pug', async () => {
 	const { violations } = await mlTest(
 		'button(type=buttonType)',
