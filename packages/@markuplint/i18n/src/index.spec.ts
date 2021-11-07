6b6a69efdfd0198469915df59caeb325e02825eb
yusukehirao <yusukehirao@me.com>
2021-11-07 19:40:40 +0900

    Remove the async keyword.

--- packages/@markuplint/i18n/src/index.spec.ts
+++ packages/@markuplint/i18n/src/index.spec.ts
@@ -3,7 +3,7 @@ import { translator } from './';
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const ja = require('../locales/ja.json');
 
-test('Complementize', async () => {
+test('Complementize', () => {
 	const t = translator(ja);
 
 	expect(t('{0} is {1}', 'attribute', 'deprecated')).toBe('属性は非推奨です');
@@ -12,7 +12,7 @@ test('Complementize', async () => {
 	expect(t('{0} is {1:c}', 'attribute', 'obsolete')).toBe('属性は廃止されています');
 });
 
-test('Nesting', async () => {
+test('Nesting', () => {
 	const t = translator();
 	expect(t('{0} must be {1}', t('{0} of {1}', 'attribute names', 'HTML elements'), 'lowercase')).toBe(
 		'attribute names of HTML elements must be lowercase',
