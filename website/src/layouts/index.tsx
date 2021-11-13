a96ed27cd2ba481d46f67b1ad0b47cb3dc6bc660
yusukehirao <yusukehirao@me.com>
2021-11-13 22:17:02 +0900

    Change indent width in the website directory.

--- website/src/layouts/index.tsx
+++ website/src/layouts/index.tsx
@@ -6,95 +6,95 @@ import Layout from '../components/Layout';
 import BasicMeta from '../components/meta/BasicMeta';
 
 type Props = {
-	title: string;
-	back?: {
-		title: string;
-		href: string;
-	};
-	next?: {
-		title: string;
-		href: string;
-	};
+  title: string;
+  back?: {
+    title: string;
+    href: string;
+  };
+  next?: {
+    title: string;
+    href: string;
+  };
 };
 
 export default function Index({ children, frontMatter }: PropsWithChildren<{ frontMatter: Props }>) {
-	const { title, back, next } = frontMatter;
-	return (
-		<Layout>
-			<BasicMeta title={title} />
-			<h1>{title}</h1>
-			<div>{children}</div>
-			{(back || next) && (
-				<nav className="step-nav" aria-label="Step">
-					<div className="step-nav-back">
-						{back && (
-							<Link href={`./${back.href}/`}>
-								<a href={`./${back.href}/`}>
-									<span>Back:</span>
-									<span>{back.title}</span>
-								</a>
-							</Link>
-						)}
-					</div>
-					<div className="step-nav-next">
-						{next && (
-							<Link href={`./${next.href}/`}>
-								<a href={`./${next.href}/`}>
-									<span>Next:</span>
-									<span>{next.title}</span>
-								</a>
-							</Link>
-						)}
-					</div>
-				</nav>
-			)}
-			<style jsx>
-				{`
-					.step-nav {
-						display: flex;
-						justify-content: space-between;
-						margin: 2em 0 0;
-					}
+  const { title, back, next } = frontMatter;
+  return (
+    <Layout>
+      <BasicMeta title={title} />
+      <h1>{title}</h1>
+      <div>{children}</div>
+      {(back || next) && (
+        <nav className="step-nav" aria-label="Step">
+          <div className="step-nav-back">
+            {back && (
+              <Link href={`./${back.href}/`}>
+                <a href={`./${back.href}/`}>
+                  <span>Back:</span>
+                  <span>{back.title}</span>
+                </a>
+              </Link>
+            )}
+          </div>
+          <div className="step-nav-next">
+            {next && (
+              <Link href={`./${next.href}/`}>
+                <a href={`./${next.href}/`}>
+                  <span>Next:</span>
+                  <span>{next.title}</span>
+                </a>
+              </Link>
+            )}
+          </div>
+        </nav>
+      )}
+      <style jsx>
+        {`
+          .step-nav {
+            display: flex;
+            justify-content: space-between;
+            margin: 2em 0 0;
+          }
 
-					.step-nav-back,
-					.step-nav-next {
-						display: flex;
-						flex: 0 1 auto;
-					}
+          .step-nav-back,
+          .step-nav-next {
+            display: flex;
+            flex: 0 1 auto;
+          }
 
-					.step-nav-back {
-						margin: 0 0.2em 0 0;
-					}
+          .step-nav-back {
+            margin: 0 0.2em 0 0;
+          }
 
-					.step-nav-next {
-						margin: 0 0 0 0.2em;
-						justify-content: flex-end;
-					}
+          .step-nav-next {
+            margin: 0 0 0 0.2em;
+            justify-content: flex-end;
+          }
 
-					.step-nav a {
-						display: block;
-						padding: 0.5em 1em;
-						background: var(--base-color-front);
-						box-shadow: 0 1px 2px 0 var(--shadow);
-						color: inherit;
-						text-decoration: none;
-						border-radius: 3px;
-					}
+          .step-nav a {
+            display: block;
+            padding: 0.5em 1em;
+            background: var(--base-color-front);
+            box-shadow: 0 1px 2px 0 var(--shadow);
+            color: inherit;
+            text-decoration: none;
+            border-radius: 3px;
+          }
 
-					.step-nav a span {
-						display: inline-block;
-					}
+          .step-nav a span {
+            display: inline-block;
+          }
 
-					.step-nav a span:first-child {
-						opacity: 0.8;
-						margin: 0 0.3em 0 0;
-					}
+          .step-nav a span:first-child {
+            opacity: 0.8;
+            margin: 0 0.3em 0 0;
+          }
 
-					.step-nav a span:last-child {
-						font-weight: 700;
-					}
-				`}
-			</style>
-		</Layout>
-	);
+          .step-nav a span:last-child {
+            font-weight: 700;
+          }
+        `}
+      </style>
+    </Layout>
+  );
 }
