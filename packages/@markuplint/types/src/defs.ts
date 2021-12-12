811f0bcc024eccac67b4f74edb63e5d17982df60
yusukehirao <yusukehirao@me.com>
2021-12-12 16:39:02 +0900

    Fix range out of order in character class (Invalid regular expression)

--- packages/@markuplint/types/src/defs.ts
+++ packages/@markuplint/types/src/defs.ts
@@ -98,11 +98,11 @@ export const types: Defs = {
 		is: value => {
 			// NameStartChar ::= ":" | [A-Z] | "_" | [a-z] | [#xC0-#xD6] | [#xD8-#xF6] | [#xF8-#x2FF] | [#x370-#x37D] | [#x37F-#x1FFF] | [#x200C-#x200D] | [#x2070-#x218F] | [#x2C00-#x2FEF] | [#x3001-#xD7FF] | [#xF900-#xFDCF] | [#xFDF0-#xFFFD] | [#x10000-#xEFFFF]
 			const nameStartChar =
-				/[:A-Z_a-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\u{10000}-\u{EFFFF}]/;
+				/[:A-Z_a-z\u00C0-\u00D6\u00D8-\u00F6\u00F8-\u02FF\u0370-\u037D\u037F-\u1FFF\u200C-\u200D\u2070-\u218F\u2C00-\u2FEF\u3001-\uD7FF\uF900-\uFDCF\uFDF0-\uFFFD\u{10000}-\u{EFFFF}]/u;
 			// NameChar ::= NameStartChar | "-" | "." | [0-9] | #xB7 | [#x0300-#x036F] | [#x203F-#x2040]
 			const nameCharTail = /-|[.0-9\u00B7]|[\u0300-\u036F\u203F-\u2040]/;
 			// Name ::= NameStartChar (NameChar)*
-			const name = RegExp(`(?:${nameStartChar.source})(?:${nameCharTail})*`);
+			const name = RegExp(`(?:${nameStartChar.source})(?:${nameCharTail})*`, 'u');
 			return name.test(value) ? matched() : unmatched(value, 'unexpected-token');
 		},
 	},
