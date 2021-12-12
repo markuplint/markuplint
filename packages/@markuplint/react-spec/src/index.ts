0a9b20e4f30c56330bb970f26d5da3c246ce95cc
yusukehirao <yusukehirao@me.com>
2021-12-12 16:20:29 +0900

    Update specs for the lang.

--- packages/@markuplint/react-spec/src/index.ts
+++ packages/@markuplint/react-spec/src/index.ts
@@ -1,98 +1,117 @@
 import type { ExtendedSpec } from '@markuplint/ml-spec';
 
 const spec: ExtendedSpec = {
-	cites: ['https://reactjs.org/docs/dom-elements.html'],
 	def: {
 		'#globalAttrs': {
-			'#extends': [
-				{
-					name: 'key',
-					type: 'String',
-					description: 'A special attribute for list rendering',
+			'#extends': {
+				/**
+				 * A special attribute for list rendering
+				 */
+				key: {
+					type: 'Any',
 				},
-				{
-					name: 'ref',
-					type: 'String',
-					description: 'A special attribute for accessing child component instances and child elements',
+				/**
+				 * A special attribute for accessing child component instances
+				 * and child elements
+				 */
+				ref: {
+					type: 'Any',
 				},
-				{
-					name: 'dangerouslySetInnerHTML',
-					type: 'String',
-					description: 'React’s replacement for using innerHTML in the browser DOM',
+				/**
+				 * React’s replacement for using innerHTML in the browser DOM
+				 */
+				dangerouslySetInnerHTML: {
+					type: 'Any',
 				},
-				{
-					name: 'suppressContentEditableWarning',
+				/**
+				 * Normally, there is a warning when an element with children
+				 * is also marked as contentEditable, because it won’t work.
+				 * This attribute suppresses that warning.
+				 */
+				suppressContentEditableWarning: {
 					type: 'Boolean',
-					description:
-						'Normally, there is a warning when an element with children is also marked as contentEditable, because it won’t work. This attribute suppresses that warning.',
 				},
-				{
-					name: 'suppressHydrationWarning',
+				/**
+				 * If you set suppressHydrationWarning to true,
+				 * React will not warn you about mismatches
+				 * in the attributes and the content of that element.
+				 */
+				suppressHydrationWarning: {
 					type: 'Boolean',
-					description:
-						'If you set suppressHydrationWarning to true, React will not warn you about mismatches in the attributes and the content of that element.',
 				},
-			],
+			},
 		},
 	},
 	specs: [
 		{
 			name: 'input',
-			attributes: [
-				{
-					name: 'defaultChecked',
+			attributes: {
+				/**
+				 * defaultChecked is the uncontrolled equivalent,
+				 * which sets whether the component is checked
+				 * when it is first mounted.
+				 */
+				defaultChecked: {
 					type: 'Boolean',
-					description:
-						'defaultChecked is the uncontrolled equivalent, which sets whether the component is checked when it is first mounted.',
 					caseSensitive: true,
 					condition: {
 						self: ['[type=checkbox]', '[type=radio]'],
 					},
 				},
-				{
-					name: 'defaultValue',
-					type: 'String',
+				/**
+				 * defaultValue is the uncontrolled equivalent,
+				 * which sets the value of the component
+				 * when it is first mounted.
+				 */
+				defaultValue: {
+					type: 'Any',
 					caseSensitive: true,
-					description:
-						'defaultValue is the uncontrolled equivalent, which sets the value of the component when it is first mounted.',
 				},
-			],
+			},
 		},
 		{
 			name: 'select',
-			attributes: [
-				{
-					name: 'value',
-					type: 'String',
-					description:
-						'The value attribute is supported by <input>, <select> and <textarea> components. You can use it to set the value of the component.',
+			attributes: {
+				/**
+				 * The value attribute is supported by
+				 * <input>, <select> and <textarea> components.
+				 * You can use it to set the value of the component.
+				 */
+				value: {
+					type: 'Any',
 				},
-				{
-					name: 'defaultValue',
-					type: 'String',
+				/**
+				 * defaultValue is the uncontrolled equivalent,
+				 * which sets the value of the component
+				 * when it is first mounted.
+				 */
+				defaultValue: {
+					type: 'Any',
 					caseSensitive: true,
-					description:
-						'defaultValue is the uncontrolled equivalent, which sets the value of the component when it is first mounted.',
 				},
-			],
+			},
 		},
 		{
 			name: 'textarea',
-			attributes: [
-				{
-					name: 'value',
-					type: 'String',
-					description:
-						'The value attribute is supported by <input>, <select> and <textarea> components. You can use it to set the value of the component.',
+			attributes: {
+				/**
+				 * The value attribute is supported by
+				 * <input>, <select> and <textarea> components.
+				 * You can use it to set the value of the component.
+				 */
+				value: {
+					type: 'Any',
 				},
-				{
-					name: 'defaultValue',
-					type: 'String',
+				/**
+				 * defaultValue is the uncontrolled equivalent,
+				 * which sets the value of the component
+				 * when it is first mounted.
+				 */
+				defaultValue: {
+					type: 'Any',
 					caseSensitive: true,
-					description:
-						'defaultValue is the uncontrolled equivalent, which sets the value of the component when it is first mounted.',
 				},
-			],
+			},
 		},
 	],
 };
