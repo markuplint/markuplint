d0aa647209ead77c9801769d3ad0090a50fe4680
yusukehirao <yusukehirao@me.com>
2022-05-16 21:12:30 +0900

    Fix the problem that failed to parse when starting the head or body tags.

---
+++ packages/@markuplint/html-parser/src/optimize-starts-head-or-body.ts
@@ -0,0 +1,56 @@
+import type { MLASTNode } from '@markuplint/ml-ast';
+
+const UNDUPLICATED_CHAR = '\uFFFD';
+
+export function isStartsHeadTagOrBodyTag(rawCode: string) {
+	return /^\s*<(?:head|body)>/i.test(rawCode);
+}
+
+export function optimizeStartsHeadTagOrBodyTagSetup(rawCode: string) {
+	const heads: string[] = [];
+	const bodies: string[] = [];
+	const code = rawCode.replace(
+		// eslint-disable-next-line no-control-regex
+		/(?<=<\/?)(?:head|body)(?=\u0009|\u000A|\u000C|\u0020|\/|>|\u0000)/gi,
+		tag => {
+			const prefix = `x-${UNDUPLICATED_CHAR}`;
+			let name: string;
+			if (/^head$/i.test(tag)) {
+				name = `${prefix}h`;
+				heads.push(tag);
+			} else if (/^body/i.test(tag)) {
+				name = `${prefix}b`;
+				bodies.push(tag);
+			} else {
+				throw new Error('Never error');
+			}
+			return name;
+		},
+	);
+	return {
+		code,
+		heads,
+		bodies,
+	};
+}
+
+export function optimizeStartsHeadTagOrBodyTagResume(
+	nodeList: MLASTNode[],
+	replacements: ReturnType<typeof optimizeStartsHeadTagOrBodyTagSetup>,
+) {
+	nodeList.forEach(node => {
+		if (!node.nodeName.startsWith(`x-${UNDUPLICATED_CHAR}`)) {
+			return;
+		}
+		const realName =
+			node.nodeName === `x-${UNDUPLICATED_CHAR}h` ? replacements.heads.shift() : replacements.bodies.shift();
+		if (!realName) {
+			return;
+		}
+		node.raw = node.raw.replace(node.nodeName, realName);
+		node.nodeName = realName;
+		if (node.type === 'starttag') {
+			node.isCustomElement = false;
+		}
+	});
+}
