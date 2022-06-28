656403e0d50e637fdd685af5b4ddfd61361e5c53
yusukehirao <yusukehirao@me.com>
2022-06-28 12:38:38 +0900

    Fix to import presets

--- packages/@markuplint/ml-core/src/configs.ts
+++ packages/@markuplint/ml-core/src/configs.ts
@@ -1,6 +1,6 @@
 import type { Config } from '@markuplint/ml-config';
 
 export async function getPreset(name: string): Promise<Config> {
-	const json = await import(`@markuplint/config-presets/${name}.json`);
+	const json = await import(`@markuplint/config-presets/preset.${name}.json`);
 	return json.default as Config;
 }
