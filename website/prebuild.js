e5e1a74024b396c309b6009bafbb59e5fc155914
kagan <intend.on.games+git@gmail.com>
2022-10-30 14:43:16 +0900

    Fix prebuild script

--- website/prebuild.js
+++ website/prebuild.js
@@ -11,7 +11,6 @@ const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 const ruleDir = path.resolve(process.cwd(), '../packages/@markuplint/rules/src/');
 const globPath = path.resolve(ruleDir, '**/README{,.ja}.md');
-const distDir = path.resolve(__dirname, './src/pages/rules');
 
 async function copy() {
   const rulesMdList = await asyncGlob(globPath);
@@ -19,7 +18,11 @@ async function copy() {
   await Promise.all(
     rulesMdList.map(async md => {
       const dirname = path.basename(path.dirname(md));
-      const dist = path.resolve(distDir, `${dirname}${md.endsWith('.ja.md') ? '.ja' : ''}.mdx`);
+      const locale = md.endsWith('.ja.md') ? 'ja' : 'en';
+      const dist =
+        locale === 'en'
+          ? path.resolve(__dirname, './docs/rules', `${dirname}.mdx`)
+          : path.resolve(__dirname, `./i18n/${locale}/docusaurus-plugin-content-docs/current/rules`, `${dirname}.mdx`);
       const content = await readFile(md, { encoding: 'utf-8' });
       const rewrote = content.replace(/\(https:\/\/markuplint\.dev\//g, '(/');
       await writeFile(dist, rewrote, { encoding: 'utf-8' });
