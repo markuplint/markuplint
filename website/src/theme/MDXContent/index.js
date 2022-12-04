476377652ca71f6bac364fa40b24a92d5142717e
kagan <intend.on.games+git@gmail.com>
2022-12-04 17:46:21 +0900

    Fix can't use `.md` or `.mdx` in pages folder

--- website/src/theme/MDXContent/index.js
+++ website/src/theme/MDXContent/index.js
@@ -1,15 +1,11 @@
-import { useDoc } from '@docusaurus/theme-common/internal';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import MDXContent from '@theme-original/MDXContent';
 import Admonition from '@theme/Admonition';
 import React from 'react';
 
 export default function MDXContentWrapper(props) {
-  const {
-    metadata: { source },
-  } = useDoc();
   const { i18n } = useDocusaurusContext();
-
+  const source = props.children.type.metadata.source;
   return (
     <>
       {i18n.currentLocale === 'ja' && !source.startsWith('@site/i18n') && (
