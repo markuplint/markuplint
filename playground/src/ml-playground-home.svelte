941a19ea227fd392e5fe665a860ab41314a75c87
yusukehirao <yusukehirao@me.com>
2022-12-18 18:39:56 +0900

    Fix to hardcode because didn't resolve config presets.

--- playground/src/ml-playground-home.svelte
+++ playground/src/ml-playground-home.svelte
@@ -1,8 +1,39 @@
 <script>
 	import MlPlayground from './ml-playground.svelte';
 
-	const rulesJSON = require('@markuplint/ml-core/markuplint-recommended.json');
-	const ruleset = JSON.stringify(rulesJSON);
+	const ruleset = JSON.stringify({
+		rules: {
+			"attr-duplication": true,
+			"attr-value-quotes": true,
+			"case-sensitive-attr-name": true,
+			"case-sensitive-tag-name": true,
+			"character-reference": true,
+			"class-naming": true,
+			"deprecated-attr": true,
+			"deprecated-element": true,
+			"disallowed-element": true,
+			"doctype": true,
+			"end-tag": true,
+			"id-duplication": true,
+			"ineffective-attr": true,
+			"invalid-attr": true,
+			"label-has-control": true,
+			"landmark-roles": true,
+			"no-boolean-attr-value": true,
+			"no-default-value": true,
+			"no-empty-palpable-content": true,
+			"no-hard-code-id": true,
+			"no-refer-to-non-existent-id": true,
+			"no-use-event-handler-attr": true,
+			"permitted-contents": true,
+			"require-accessible-name": true,
+			"required-attr": true,
+			"required-element": true,
+			"required-h1": true,
+			"use-list": true,
+			"wai-aria": true,
+		}
+	});
 </script>
 
 <style>
