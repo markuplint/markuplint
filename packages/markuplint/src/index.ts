a524f7a6c6c7ba974a589fc2a026f79bccdc5682
yusukehirao <yusukehirao@me.com>
2023-01-09 19:52:40 +0900

    Remove v1 API.

--- packages/markuplint/src/index.ts
+++ packages/markuplint/src/index.ts
@@ -2,8 +2,3 @@ export { MLEngine } from './api';
 export * from './i18n';
 export * from './testing-tool';
 export * from './types';
-
-/**
- * @deprecated
- */
-export * from './v1';
--- packages/markuplint/src/v1.ts
+++
@@ -1,4 +0,0 @@
-/**
- * @deprecated
- */
-export { lint_v1 as exec } from './api/v1';
