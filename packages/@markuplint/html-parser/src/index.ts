4a2ce4be06907b1d9e6cd7568e5753b65d6e9615
yusukehirao <yusukehirao@me.com>
2023-03-19 15:04:09 +0900

    export `createTree`

--- packages/@markuplint/html-parser/src/index.ts
+++ packages/@markuplint/html-parser/src/index.ts
@@ -1,5 +1,6 @@
-export { default as isDocumentFragment } from './is-document-fragment';
-export { parse } from './parse';
-export { getNamespace } from './get-namespace';
-export { default as parseRawTag } from './parse-raw-tag';
 export { default as attrTokenizer } from './attr-tokenizer';
+export { default as isDocumentFragment } from './is-document-fragment';
+export { default as parseRawTag } from './parse-raw-tag';
+export { getNamespace } from './get-namespace';
+export { parse } from './parse';
+export { createTree } from './create-tree';
