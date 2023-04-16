a026562bcac77f7f89a6c4f3f94cca4c9fa97575
yusukehirao <yusukehirao@me.com>
2023-04-16 12:21:11 +0900

    Add tests for ARIA version 1.3.

---
+++ packages/@markuplint/ml-spec/src/dom-traverse/get-implicit-role.spec.ts
@@ -0,0 +1,40 @@
+import specs from '@markuplint/html-spec';
+import { createSelector } from '@markuplint/selector';
+import { createJSDOMElement } from '@markuplint/test-tools';
+
+import { getImplicitRole } from './get-implicit-role';
+
+function _(html: string, selector?: string) {
+	return createJSDOMElement(html, selector, function (selector) {
+		// JSDOM supports no level 4 selectors yet.
+		return createSelector(selector, specs).match(this) !== false;
+	});
+}
+
+describe('1.2', () => {
+	test('<button>', () => {
+		const el = _('<button></button>');
+		const role = getImplicitRole(specs, el, '1.2');
+		expect(role.role?.name).toBe('button');
+	});
+
+	test('<td>', () => {
+		const el = _('<table><tr><td></td></tr></table>', 'td');
+		const role = getImplicitRole(specs, el, '1.2');
+		expect(role.role?.name).toBe('cell');
+	});
+});
+
+describe('1.3', () => {
+	test('<button>', () => {
+		const el = _('<button></button>');
+		const role = getImplicitRole(specs, el, '1.3');
+		expect(role.role?.name).toBe('button');
+	});
+
+	test('<td>', () => {
+		const el = _('<table><tr><td></td></tr></table>', 'td');
+		const role = getImplicitRole(specs, el, '1.3');
+		expect(role.role?.name).toBe('cell');
+	});
+});
