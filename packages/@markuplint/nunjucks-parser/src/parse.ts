b597e71b9bb36b50cf10a91e6c9501b5ff053396
yusukehirao <yusukehirao@me.com>
2023-04-30 12:10:57 +0900

    Fix template engine parsers to accept parser options.

--- packages/@markuplint/nunjucks-parser/src/parse.ts
+++ packages/@markuplint/nunjucks-parser/src/parse.ts
@@ -3,7 +3,7 @@ import type { Parse } from '@markuplint/ml-ast';
 import { parse as htmlParse } from '@markuplint/html-parser';
 import { ignoreBlock, restoreNode } from '@markuplint/parser-utils';
 
-export const parse: Parse = rawCode => {
+export const parse: Parse = (rawCode, options) => {
 	const blocks = ignoreBlock(rawCode, [
 		{
 			type: 'nunjucks-block',
@@ -21,7 +21,7 @@ export const parse: Parse = rawCode => {
 			end: /#}/,
 		},
 	]);
-	const doc = htmlParse(blocks.replaced);
+	const doc = htmlParse(blocks.replaced, options);
 	doc.nodeList = restoreNode(doc.nodeList, blocks);
 
 	return doc;
