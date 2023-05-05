2a88c16b8e32f1476187eb6fd6e57d4f4366671d
yusukehirao <yusukehirao@me.com>
2023-05-05 16:11:11 +0900

    Add isolated test environments

---
+++ test/isolated-env/typescript/index.ts
@@ -0,0 +1,15 @@
+import { MLEngine } from 'markuplint';
+
+async function main() {
+	const file = await MLEngine.toMLFile('../../fixture/002.html');
+
+	if (!file) {
+		return;
+	}
+
+	const engine = new MLEngine(file, {
+		locale: 'en',
+	});
+
+	await engine.exec();
+}
