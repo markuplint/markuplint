38940336f33c828b29e8f2331dd8085f8e978a79
yusukehirao <yusukehirao@me.com>
2023-07-22 20:36:57 +0900

    Fix getting a module as JSON on ESM.

--- packages/markuplint/src/i18n.ts
+++ packages/markuplint/src/i18n.ts
@@ -2,6 +2,8 @@ import type { LocaleSet } from '@markuplint/i18n';
 
 import { osLocale } from 'os-locale';
 
+import { getJsonModule } from './get-json-module.js';
+
 let cachedLocale: string | null = null;
 
 async function getLocale() {
@@ -14,12 +16,8 @@ async function getLocale() {
 export async function i18n(locale?: string): Promise<LocaleSet> {
 	locale = locale ?? (await getLocale()) ?? 'en';
 	const langCode = locale.split('-')[0] ?? locale;
-	const loadLocaleSet: LocaleSet | null = await import(`@markuplint/i18n/locales/${langCode}.json`).catch(() => null);
-	// @ts-ignore
-	const localeSet: LocaleSet =
-		loadLocaleSet ??
-		// @ts-ignore
-		(await import('@markuplint/i18n/locales/en.json'));
+	const loadLocaleSet = getJsonModule<LocaleSet>(`@markuplint/i18n/locales/${langCode}.json`);
+	const localeSet: LocaleSet = loadLocaleSet ?? getJsonModule('@markuplint/i18n/locales/en.json')!;
 	return {
 		...localeSet,
 		locale: loadLocaleSet ? langCode : 'en',
