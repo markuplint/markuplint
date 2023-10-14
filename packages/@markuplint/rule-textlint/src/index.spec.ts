2163399e6369850ac1537e6dc1c2793ec970466a
yusukehirao <yusukehirao@me.com>
2023-10-14 17:07:17 +0900

    Improve `rule-textlint`

--- packages/@markuplint/rule-textlint/src/index.spec.ts
+++ packages/@markuplint/rule-textlint/src/index.spec.ts
@@ -1,56 +1,53 @@
+import fs from 'node:fs/promises';
 import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
 import { mlTest, mlTestFile } from 'markuplint';
-// @ts-ignore
 import Prh from 'textlint-rule-prh';
 import { test, expect } from 'vitest';
-
-import { text } from './test-utils.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-test('is test 1', async () => {
-	const { violations } = await mlTest(
-		text,
-		{
-			rules: {
-				textlint: {
-					options: {
-						rules: [
-							{
-								ruleId: 'prh',
-								rule: Prh,
-								options: {
-									rulePaths: [path.resolve(__dirname, '../', 'prh.yml')],
-								},
+const textFilePath = path.resolve(__dirname, '..', 'test', 'fixture.html');
+const text = await fs.readFile(textFilePath, 'utf-8');
+
+test('Manual', async () => {
+	const { violations } = await mlTest(text, {
+		rules: {
+			textlint: {
+				options: {
+					rules: [
+						{
+							ruleId: 'prh',
+							rule: Prh,
+							options: {
+								rulePaths: [path.resolve(__dirname, '..', 'prh.yml')],
 							},
-						],
-					},
+						},
+					],
 				},
 			},
 		},
-		// Auto loading
-	);
+	});
 
 	expect(violations).toStrictEqual([
 		{
 			severity: 'warning',
 			ruleId: 'textlint',
 			line: 11,
-			col: 20,
+			col: 19,
 			raw: 'jquery',
 			message: 'Invalid text: jquery => jQuery',
 		},
 	]);
 });
 
-test('is test 2', async () => {
+test('Cannot autoload', async () => {
 	const { violations } = await mlTestFile(
 		{
 			sourceCode: text,
-			name: path.resolve('test/fixture/textlint/test.html'),
+			name: textFilePath,
 		},
 		{
 			rules: {
@@ -59,14 +56,5 @@ test('is test 2', async () => {
 		},
 	);
 
-	expect(violations).toStrictEqual([
-		{
-			severity: 'warning',
-			ruleId: 'textlint',
-			line: 11,
-			col: 20,
-			raw: 'jquery',
-			message: 'Invalid text: jquery => jQuery',
-		},
-	]);
+	expect(violations.length).toEqual(0);
 });
