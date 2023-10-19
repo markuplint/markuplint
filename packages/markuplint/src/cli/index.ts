92fd3396bc59199aa857f043a1bcaf602de8fd02
yusukehirao <yusukehirao@me.com>
2023-10-19 21:35:42 +0900

    Fix according to `unicorn/prefer-top-level-await`

--- packages/markuplint/src/cli/index.ts
+++ packages/markuplint/src/cli/index.ts
@@ -10,77 +10,73 @@ import search from './search/index.js';
 
 /* eslint-disable unicorn/no-process-exit */
 
-// eslint-disable-next-line @typescript-eslint/no-floating-promises
-(async () => {
-	if (cli.flags.v) {
-		cli.showVersion(); // And exit successfully.
-	}
+if (cli.flags.v) {
+	cli.showVersion(); // And exit successfully.
+}
 
-	if (cli.flags.h) {
-		cli.showHelp(0); // And exit successfully.
-	}
+if (cli.flags.h) {
+	cli.showHelp(0); // And exit successfully.
+}
 
-	if (cli.flags.verbose) {
-		verbosely();
-	}
+if (cli.flags.verbose) {
+	verbosely();
+}
 
-	if (cli.flags.init) {
-		await initialize().catch(error => {
+if (cli.flags.init) {
+	await initialize().catch(error => {
+		process.stderr.write(error + '\n');
+		process.exit(1);
+	});
+	process.exit(0);
+}
+
+if (cli.flags.createRule) {
+	await createRule().catch(error => {
+		process.stderr.write(error + '\n');
+		process.exit(1);
+	});
+	process.exit(0);
+}
+
+const files = cli.input;
+if (files.length > 0) {
+	if (cli.flags.search) {
+		await search(files, cli.flags, cli.flags.search).catch(error => {
 			process.stderr.write(error + '\n');
 			process.exit(1);
 		});
-		return;
+		process.exit(0);
 	}
 
-	if (cli.flags.createRule) {
-		await createRule().catch(error => {
-			process.stderr.write(error + '\n');
-			process.exit(1);
-		});
-		return;
-	}
+	const hasError = await command(files, cli.flags).catch(error => {
+		throw error;
+	});
+	process.exit(hasError ? 1 : 0);
+}
 
-	const files = cli.input;
-	if (files.length > 0) {
-		if (cli.flags.search) {
-			await search(files, cli.flags, cli.flags.search).catch(error => {
-				process.stderr.write(error + '\n');
-				process.exit(1);
-			});
-			return;
-		}
-
-		const hasError = await command(files, cli.flags).catch(error => {
-			throw error;
-		});
-		process.exit(hasError ? 1 : 0);
-	}
-
-	if (usePipe()) {
-		getStdin()
-			.then(async stdin => {
-				if (stdin) {
-					const hasError = await command([{ sourceCode: stdin }], {
-						...cli.flags,
-						ignoreExt: true,
-					}).catch(error => {
-						process.stderr.write(error + '\n');
-						process.exit(1);
-					});
-					process.exit(hasError ? 1 : 0);
-				}
-				// result is empty
-				cli.showHelp(1);
-			})
-			.catch(error => {
-				// eslint-disable-next-line no-console
-				console.warn(error);
-				process.exit(1);
-			});
-	} else {
+if (usePipe()) {
+	const stdin = await getStdin().catch(error => {
+		// eslint-disable-next-line no-console
+		console.warn(error);
+		process.exit(1);
+	});
+	if (!stdin) {
+		// result is empty
 		cli.showHelp(1);
 	}
-})();
+
+	const hasError = await command([{ sourceCode: stdin }], {
+		...cli.flags,
+		ignoreExt: true,
+	}).catch(error => {
+		process.stderr.write(error + '\n');
+		process.exit(1);
+	});
+	process.exit(hasError ? 1 : 0);
+}
+
+// No arguments
+cli.showHelp(1);
 
 /* eslint-enable unicorn/no-process-exit */
 
