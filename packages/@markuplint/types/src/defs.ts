d6041a12119c28b5c228ebd12b55ad7dc8254a4b
yusukehirao <yusukehirao@me.com>
2024-01-04 00:07:12 +0900

    Support legacy SVG `transform` attribute value syntax

--- packages/@markuplint/types/src/defs.ts
+++ packages/@markuplint/types/src/defs.ts
@@ -913,6 +913,33 @@ export const types: Defs = {
 	},
 };
 
+export const overrides: Record<string, string> = {
+	// Alias
+	'legacy-length-percentage': '<length> | <percentage> | <svg-length>',
+	'legacy-angle': '<angle> | <zero> | <number>',
+
+	/**
+	 * @see https://www.w3.org/TR/css-transforms-1/#funcdef-transform-translate
+	 */
+	'translate()':
+		'translate( <legacy-length-percentage> , <legacy-length-percentage>? ) | translate( <legacy-length-percentage> <legacy-length-percentage>? )',
+
+	/**
+	 * @see https://www.w3.org/TR/css-transforms-1/#funcdef-transform-scale
+	 */
+	'scale()': 'scale( [ <number> | <percentage> ]#{1,2} )',
+
+	/**
+	 * @see https://www.w3.org/TR/css-transforms-1/#funcdef-transform-rotate
+	 */
+	'rotate()': 'rotate( <legacy-angle> )',
+
+	/**
+	 * @see https://www.w3.org/TR/css-transforms-1/#funcdef-transform-skew
+	 */
+	'skew()': 'skew( <legacy-angle> , <legacy-angle>? ) | skew( <legacy-angle> <legacy-angle>? )',
+};
+
 export const tokenizers: Record<string, CssSyntaxTokenizer> = {
 	// RFC
 	// https://tools.ietf.org/rfc/bcp/bcp47.html
