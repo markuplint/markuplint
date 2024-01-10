9fa7b6e0d2505b346f5c09d07092d829b21c5152
kagan <intend.on.games+git@gmail.com>
2024-01-11 01:19:25 +0900

    Fix flaky screenshot

--- website/tests/screenshot.spec.ts
+++ website/tests/screenshot.spec.ts
@@ -12,6 +12,14 @@ const TARGET_PAGES = [
 for (const pagePath of TARGET_PAGES) {
   test(`screenshot ${pagePath}`, async ({ page }) => {
     await page.goto(pagePath);
+    // hide images in order to avoid flaky screenshot
+    await page.addStyleTag({
+      content: /* css */ `
+        img[loading="lazy"] {
+          visibility: hidden;
+        }
+      `,
+    });
     await page.waitForSelector('footer'); // without this, the whole page cannot be captured
     await page.screenshot({
       path: `__screenshots__/${(pagePath === '/' ? '/index' : pagePath).replaceAll('/', '_')}.png`,
