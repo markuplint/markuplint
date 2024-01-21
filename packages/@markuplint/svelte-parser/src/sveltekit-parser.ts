343f1badad5c184f8ef38c5faaebcf570a6f52b5
yusukehirao <yusukehirao@me.com>
2024-01-21 16:09:07 +0900

    Enhancing `svelte-parser` with newly created `Parser` class

--- packages/@markuplint/svelte-parser/src/sveltekit-parser.ts
+++ packages/@markuplint/svelte-parser/src/sveltekit-parser.ts
@@ -1,18 +1,17 @@
-import type { Parse } from '@markuplint/ml-ast';
+import { HtmlParser } from '@markuplint/html-parser';
 
-import { parse as htmlParse } from '@markuplint/html-parser';
-import { ignoreBlock, restoreNode } from '@markuplint/parser-utils';
+class SvelteKitTemplateParser extends HtmlParser {
+	constructor() {
+		super({
+			ignoreTags: [
+				{
+					type: 'sveltekit-placeholder',
+					start: '%sveltekit.',
+					end: '%',
+				},
+			],
+		});
+	}
+}
 
-export const parse: Parse = (rawCode, options) => {
-	const blocks = ignoreBlock(rawCode, [
-		{
-			type: 'sveltekit-placeholder',
-			start: '%sveltekit.',
-			end: '%',
-		},
-	]);
-	const doc = htmlParse(blocks.replaced, options);
-	doc.nodeList = restoreNode(doc.nodeList, blocks);
-
-	return doc;
-};
+export const parser = new SvelteKitTemplateParser();
