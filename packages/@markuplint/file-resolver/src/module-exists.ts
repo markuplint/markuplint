107f98620a3b72da8ccf9ab63d70b480dd8e2283
yusukehirao <yusukehirao@me.com>
2024-01-28 16:20:25 +0900

    Handle `ERR_IMPORT_ASSERTION_TYPE_MISSING` error

--- packages/@markuplint/file-resolver/src/module-exists.ts
+++ packages/@markuplint/file-resolver/src/module-exists.ts
@@ -6,7 +6,18 @@ export async function moduleExists(name: string) {
 	try {
 		await import(name);
 	} catch (error) {
+		if (
+			// @ts-ignore
+			'code' in error &&
+			// @ts-ignore
+			error.code === 'ERR_IMPORT_ASSERTION_TYPE_MISSING'
+		) {
+			// It exists, but it is may be a JSON file.
+			return true;
+		}
+
 		if (error instanceof Error && /^parse failure/i.test(error.message)) {
+			// It exists, but it failed to parse.
 			return true;
 		}
 
