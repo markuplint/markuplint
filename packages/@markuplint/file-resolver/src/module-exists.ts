408633fdf939ab22c9ef016ad88438b6bb2bf069
yusukehirao <yusukehirao@me.com>
2024-01-28 16:23:43 +0900

    Add logger

--- packages/@markuplint/file-resolver/src/module-exists.ts
+++ packages/@markuplint/file-resolver/src/module-exists.ts
@@ -1,6 +1,10 @@
 import { createRequire } from 'node:module';
 
+import { log } from './debug.js';
+
 const require = createRequire(import.meta.url);
+
+const mLog = log.extend('module-exists');
 
 export async function moduleExists(name: string) {
 	try {
@@ -13,11 +17,13 @@ export async function moduleExists(name: string) {
 			error.code === 'ERR_IMPORT_ASSERTION_TYPE_MISSING'
 		) {
 			// It exists, but it is may be a JSON file.
+			mLog('Return true, but it caught Error in `import()`: %O', error);
 			return true;
 		}
 
 		if (error instanceof Error && /^parse failure/i.test(error.message)) {
 			// It exists, but it failed to parse.
+			mLog('Return true, but it caught Error in `import()`: %O', error);
 			return true;
 		}
 
@@ -32,6 +38,7 @@ export async function moduleExists(name: string) {
 			) {
 				// Even if there are issues with the fields,
 				// assume that the module exists and return true.
+				mLog('Return true, but it caught Error in `require.resolve()`: %O', error);
 				return true;
 			}
 
