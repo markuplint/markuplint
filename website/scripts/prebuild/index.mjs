3f251d54a67f9d9c63ea0ffb4fd7306f05c2fdd7
yusukehirao <yusukehirao@me.com>
2024-02-17 17:19:35 +0900

    Automatically generate a list of contributors via GitHub API

--- website/scripts/prebuild/index.mjs
+++ website/scripts/prebuild/index.mjs
@@ -1,5 +1,7 @@
 import { copyContributing } from './contributing.mjs';
+import { getContributors } from './get-contributors.mjs';
 import { createRuleDocs } from './rules.mjs';
 
 await createRuleDocs();
 await copyContributing();
+await getContributors();
