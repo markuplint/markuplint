634e1f277907f9885600cdb7d8e2c10cb60e2a31
yusukehirao <yusukehirao@me.com>
2024-05-01 18:52:44 +0900

    Use `module.createRequire` instead of Import Attribute.

--- packages/markuplint/src/version.ts
+++ packages/markuplint/src/version.ts
@@ -1,4 +1,8 @@
-// @ts-ignore
-import pkg from '../package.json' assert { type: 'json' };
+import { createRequire } from 'node:module';
 
-export const version = pkg.version;
+const require = createRequire(import.meta.url);
+
+// TODO: Use import attribute in Node.js v22 and v20.10 or later
+const pkg = require('../package.json');
+
+export const version: string = pkg.version;
