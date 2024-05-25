46c1301315e3491595f97003cb680c26a3bc7afd
yusukehirao <yusukehirao@me.com>
2024-05-25 15:54:18 +0900

    test(svelte-parser): fix AST from new parser

--- packages/@markuplint/svelte-parser/src/svelte-parser/index.spec.ts
+++ packages/@markuplint/svelte-parser/src/svelte-parser/index.spec.ts
@@ -7,16 +7,33 @@ describe('parser', () => {
 		expect(svelteParse('{#if cond}text{/if}')).toEqual([
 			{
 				type: 'IfBlock',
+				elseif: false,
 				start: 0,
 				end: 19,
-				expression: {
-					end: 9,
-					loc: { end: { column: 9, line: 1 }, start: { column: 5, line: 1 } },
+				test: {
+					type: 'Identifier',
 					name: 'cond',
 					start: 5,
-					type: 'Identifier',
+					end: 9,
+					loc: {
+						start: { line: 1, column: 5 },
+						end: { line: 1, column: 9 },
+					},
 				},
-				children: [{ data: 'text', end: 14, raw: 'text', start: 10, type: 'Text' }],
+				consequent: {
+					type: 'Fragment',
+					transparent: false,
+					nodes: [
+						{
+							type: 'Text',
+							data: 'text',
+							raw: 'text',
+							start: 10,
+							end: 14,
+						},
+					],
+				},
+				alternate: null,
 			},
 		]);
 	});
@@ -25,21 +42,44 @@ describe('parser', () => {
 		expect(svelteParse('{#if cond}text1{:else}text2{/if}')).toEqual([
 			{
 				type: 'IfBlock',
+				elseif: false,
 				start: 0,
 				end: 32,
-				expression: {
-					end: 9,
-					loc: { end: { column: 9, line: 1 }, start: { column: 5, line: 1 } },
+				test: {
+					type: 'Identifier',
 					name: 'cond',
 					start: 5,
-					type: 'Identifier',
+					end: 9,
+					loc: {
+						start: { column: 5, line: 1 },
+						end: { column: 9, line: 1 },
+					},
 				},
-				children: [{ data: 'text1', end: 15, raw: 'text1', start: 10, type: 'Text' }],
-				else: {
-					children: [{ data: 'text2', end: 27, raw: 'text2', start: 22, type: 'Text' }],
-					end: 27,
-					start: 22,
-					type: 'ElseBlock',
+				consequent: {
+					type: 'Fragment',
+					transparent: false,
+					nodes: [
+						{
+							type: 'Text',
+							data: 'text1',
+							raw: 'text1',
+							start: 10,
+							end: 15,
+						},
+					],
+				},
+				alternate: {
+					type: 'Fragment',
+					transparent: false,
+					nodes: [
+						{
+							type: 'Text',
+							start: 22,
+							end: 27,
+							data: 'text2',
+							raw: 'text2',
+						},
+					],
 				},
 			},
 		]);
@@ -49,34 +89,65 @@ describe('parser', () => {
 		expect(svelteParse('{#if cond}text1{:else if cond}text2{/if}')).toEqual([
 			{
 				type: 'IfBlock',
+				elseif: false,
 				start: 0,
 				end: 40,
-				expression: {
-					end: 9,
-					loc: { end: { column: 9, line: 1 }, start: { column: 5, line: 1 } },
+				test: {
+					type: 'Identifier',
 					name: 'cond',
 					start: 5,
-					type: 'Identifier',
+					end: 9,
+					loc: {
+						start: { column: 5, line: 1 },
+						end: { column: 9, line: 1 },
+					},
 				},
-				children: [{ data: 'text1', end: 15, raw: 'text1', start: 10, type: 'Text' }],
-				else: {
-					type: 'ElseBlock',
-					start: 30,
-					end: 35,
-					children: [
+				consequent: {
+					type: 'Fragment',
+					transparent: false,
+					nodes: [
+						{
+							type: 'Text',
+							data: 'text1',
+							raw: 'text1',
+							end: 15,
+							start: 10,
+						},
+					],
+				},
+				alternate: {
+					type: 'Fragment',
+					transparent: false,
+					nodes: [
 						{
 							type: 'IfBlock',
+							elseif: true,
 							start: 30,
 							end: 40,
-							elseif: true,
-							children: [{ data: 'text2', end: 35, raw: 'text2', start: 30, type: 'Text' }],
-							expression: {
-								end: 29,
-								loc: { end: { column: 29, line: 1 }, start: { column: 25, line: 1 } },
+							test: {
+								type: 'Identifier',
 								name: 'cond',
 								start: 25,
-								type: 'Identifier',
+								end: 29,
+								loc: {
+									start: { column: 25, line: 1 },
+									end: { column: 29, line: 1 },
+								},
 							},
+							consequent: {
+								type: 'Fragment',
+								transparent: false,
+								nodes: [
+									{
+										type: 'Text',
+										data: 'text2',
+										raw: 'text2',
+										start: 30,
+										end: 35,
+									},
+								],
+							},
+							alternate: null,
 						},
 					],
 				},
@@ -87,24 +158,41 @@ describe('parser', () => {
 	test('white spaces with if', () => {
 		expect(svelteParse('text1  {#if cond}  text2  {/if}  ')).toEqual([
 			{
+				type: 'Text',
 				data: 'text1  ',
-				end: 7,
 				raw: 'text1  ',
 				start: 0,
-				type: 'Text',
+				end: 7,
 			},
 			{
 				type: 'IfBlock',
+				elseif: false,
 				start: 7,
 				end: 31,
-				expression: {
-					end: 16,
-					loc: { end: { column: 16, line: 1 }, start: { column: 12, line: 1 } },
+				test: {
+					type: 'Identifier',
 					name: 'cond',
 					start: 12,
-					type: 'Identifier',
+					end: 16,
+					loc: {
+						start: { column: 12, line: 1 },
+						end: { column: 16, line: 1 },
+					},
 				},
-				children: [{ data: 'text2', end: 26, raw: '  text2  ', start: 17, type: 'Text' }],
+				consequent: {
+					type: 'Fragment',
+					transparent: false,
+					nodes: [
+						{
+							type: 'Text',
+							data: '  text2  ',
+							raw: '  text2  ',
+							start: 17,
+							end: 26,
+						},
+					],
+				},
+				alternate: null,
 			},
 		]);
 	});
@@ -114,29 +202,37 @@ describe('Issues', () => {
 	test('#991', () => {
 		expect(svelteParse("<CustomElement><div>Evaluation doesn't work</div></CustomElement>")).toEqual([
 			{
-				type: 'InlineComponent',
+				type: 'Component',
 				name: 'CustomElement',
 				start: 0,
 				end: 65,
 				attributes: [],
-				children: [
-					{
-						type: 'Element',
-						name: 'div',
-						start: 15,
-						end: 49,
-						attributes: [],
-						children: [
-							{
-								type: 'Text',
-								start: 20,
-								end: 43,
-								data: "Evaluation doesn't work",
-								raw: "Evaluation doesn't work",
+				fragment: {
+					type: 'Fragment',
+					transparent: true,
+					nodes: [
+						{
+							type: 'RegularElement',
+							name: 'div',
+							start: 15,
+							end: 49,
+							attributes: [],
+							fragment: {
+								type: 'Fragment',
+								transparent: true,
+								nodes: [
+									{
+										type: 'Text',
+										start: 20,
+										end: 43,
+										data: "Evaluation doesn't work",
+										raw: "Evaluation doesn't work",
+									},
+								],
 							},
-						],
-					},
-				],
+						},
+					],
+				},
 			},
 		]);
 	});
@@ -145,15 +241,37 @@ describe('Issues', () => {
 		const ast = svelteParse('{#each list as item, i (`${i}-${i}`)}{/each}');
 		expect(ast).toEqual([
 			{
-				children: [],
+				type: 'EachBlock',
+				start: 0,
+				end: 44,
+				body: {
+					type: 'Fragment',
+					transparent: false,
+					nodes: [],
+				},
 				context: {
-					end: 19,
+					type: 'Identifier',
+					typeAnnotation: undefined,
 					name: 'item',
 					start: 15,
-					type: 'Identifier',
+					end: 19,
+					loc: {
+						end: {
+							character: 19,
+							column: 19,
+							line: 1,
+						},
+						start: {
+							character: 15,
+							column: 15,
+							line: 1,
+						},
+					},
 				},
-				end: 44,
 				expression: {
+					type: 'Identifier',
+					name: 'list',
+					start: 7,
 					end: 11,
 					loc: {
 						end: {
@@ -165,15 +283,26 @@ describe('Issues', () => {
 							line: 1,
 						},
 					},
-					name: 'list',
-					start: 7,
-					type: 'Identifier',
 				},
-				index: 'i',
 				key: {
+					type: 'TemplateLiteral',
+					start: 24,
 					end: 35,
+					loc: {
+						end: {
+							column: 35,
+							line: 1,
+						},
+						start: {
+							column: 24,
+							line: 1,
+						},
+					},
 					expressions: [
 						{
+							type: 'Identifier',
+							name: 'i',
+							start: 27,
 							end: 28,
 							loc: {
 								end: {
@@ -185,11 +314,11 @@ describe('Issues', () => {
 									line: 1,
 								},
 							},
-							name: 'i',
-							start: 27,
-							type: 'Identifier',
 						},
 						{
+							type: 'Identifier',
+							name: 'i',
+							start: 32,
 							end: 33,
 							loc: {
 								end: {
@@ -201,23 +330,13 @@ describe('Issues', () => {
 									line: 1,
 								},
 							},
-							name: 'i',
-							start: 32,
-							type: 'Identifier',
 						},
 					],
-					loc: {
-						end: {
-							column: 35,
-							line: 1,
-						},
-						start: {
-							column: 24,
-							line: 1,
-						},
-					},
 					quasis: [
 						{
+							type: 'TemplateElement',
+							tail: false,
+							start: 25,
 							end: 25,
 							loc: {
 								end: {
@@ -229,15 +348,15 @@ describe('Issues', () => {
 									line: 1,
 								},
 							},
-							start: 25,
-							tail: false,
-							type: 'TemplateElement',
 							value: {
 								cooked: '',
 								raw: '',
 							},
 						},
 						{
+							type: 'TemplateElement',
+							tail: false,
+							start: 29,
 							end: 30,
 							loc: {
 								end: {
@@ -249,15 +368,15 @@ describe('Issues', () => {
 									line: 1,
 								},
 							},
-							start: 29,
-							tail: false,
-							type: 'TemplateElement',
 							value: {
 								cooked: '-',
 								raw: '-',
 							},
 						},
 						{
+							type: 'TemplateElement',
+							tail: true,
+							start: 34,
 							end: 34,
 							loc: {
 								end: {
@@ -269,20 +388,14 @@ describe('Issues', () => {
 									line: 1,
 								},
 							},
-							start: 34,
-							tail: true,
-							type: 'TemplateElement',
 							value: {
 								cooked: '',
 								raw: '',
 							},
 						},
 					],
-					start: 24,
-					type: 'TemplateLiteral',
 				},
-				start: 0,
-				type: 'EachBlock',
+				index: 'i',
 			},
 		]);
 	});
