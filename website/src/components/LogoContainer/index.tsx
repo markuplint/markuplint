68c070948a3ada48f8bf2084827fcf70b5005127
YusukeHirao <hirao@d-zero.co.jp>
2024-06-16 16:52:31 +0900

    refactor(website): logoContainer component accepts aspectRatio prop

--- website/src/components/LogoContainer/index.tsx
+++ website/src/components/LogoContainer/index.tsx
@@ -6,13 +6,15 @@ type Props = {
   src: string;
   caption: string;
   mode: 'light' | 'dark';
+  aspectRatio?: `${number}:${number}`;
 };
 
-export default function LogoContainer({ src, caption, mode }: Props) {
+export default function LogoContainer({ src, caption, mode, aspectRatio = '4:3' }: Props) {
+  const [width, height] = aspectRatio.split(':').map(Number);
   return (
     <figure className={styles.logoContainer}>
       <div className={styles.logo} data-mode={mode}>
-        <img src={src} alt="" />
+        <img src={src} alt="" width={width} height={height} />
       </div>
       <figcaption>{caption}</figcaption>
     </figure>
