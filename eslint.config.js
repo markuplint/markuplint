88b2fee7fefba92f7955448d1498315b2266a180
YusukeHirao <hirao@d-zero.co.jp>
2024-08-18 23:56:56 +0900

    chore(website): revert using `eslint-plugin-import`

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 import reactHooksPlugin from 'eslint-plugin-react-hooks';
 import { fixupPluginRules } from '@eslint/compat';
+import importPlugin from 'eslint-plugin-import';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -53,9 +54,11 @@ export default [
 		files: ['**/*.tsx'],
 		plugins: {
 			'react-hooks': fixupPluginRules(reactHooksPlugin),
+			import: importPlugin,
 		},
 		rules: {
 			...reactHooksPlugin.configs.recommended.rules,
+			...importPlugin.configs.recommended.rules,
 			'@typescript-eslint/prefer-readonly-parameter-types': 0,
 			'unicorn/filename-case': [
 				0,
@@ -63,6 +66,51 @@ export default [
 					pascalCase: true,
 				},
 			],
+
+			'import/no-unresolved': [
+				2,
+				{
+					commonjs: true,
+					ignore: ['^@docusaurus/', '^@site/', '^@theme/', '^@theme-original/'],
+				},
+			],
+			'import/no-named-as-default': 0,
+			// 'import/no-default-export': 2,
+			'import/no-default-export': 0,
+			'import/no-extraneous-dependencies': 2,
+			'import/order': [
+				'error',
+				{
+					groups: ['type', 'builtin', 'external', 'parent', 'sibling', 'index', 'object'],
+					pathGroups: [
+						{
+							pattern: '@alias/**',
+							group: 'parent',
+							position: 'before',
+						},
+					],
+					alphabetize: {
+						order: 'asc',
+					},
+					'newlines-between': 'always',
+				},
+			],
+
+			// For an issue: https://github.com/import-js/eslint-plugin-import/issues/2995
+			'import/no-named-as-default-member': 0,
+			// For an issue: https://github.com/eslint/eslint/issues/17953
+			'import/namespace': 0,
+		},
+		settings: {
+			'import/parsers': {
+				'@typescript-eslint/parser': ['.ts', '.tsx'],
+			},
+			'import/resolver': {
+				typescript: {
+					alwaysTryTypes: true,
+					project: ['packages/*/tsconfig.json'],
+				},
+			},
 		},
 	},
 	{
