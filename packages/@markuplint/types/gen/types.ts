cc9bd30e92d334bdcfa089db16a2d9f607b4149a
YusukeHirao <hirao@d-zero.co.jp>
2025-08-23 23:55:58 +0900

    refactor(types): remove unused code
    Remove unused fileURLToPath import and __dirname/__filename variables.

--- packages/@markuplint/types/gen/types.ts
+++ packages/@markuplint/types/gen/types.ts
@@ -1,15 +1,11 @@
 import fs from 'node:fs';
 import { createRequire } from 'node:module';
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 
 // @ts-ignore
 import { lexer } from 'css-tree';
 
 import { types as extendedTypes, tokenizers } from '../src/defs.js';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 const require = createRequire(import.meta.url);
 
@@ -19,7 +15,7 @@ const types = Object.keys(lexer.types).map(t => `<${t}>`);
 const specific = require('./specific-schema.json');
 
 fs.writeFileSync(
-	path.resolve(__dirname, '..', 'types.schema.json'),
+	path.resolve(import.meta.dirname, '..', 'types.schema.json'),
 	JSON.stringify({
 		$schema: 'http://json-schema.org/draft-07/schema#',
 		type: 'object',
