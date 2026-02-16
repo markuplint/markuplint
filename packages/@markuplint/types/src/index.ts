100e467bdd8bb5acd6075c2d1e12e3e33f5f9090
Yusuke Hirao <hirao@d-zero.co.jp>
2026-02-16 19:37:11 +0900

    feat(types): export getCandidate function for attribute name suggestion

--- packages/@markuplint/types/src/index.ts
+++ packages/@markuplint/types/src/index.ts
@@ -8,4 +8,5 @@
 export * from './whatwg/is-custom-element-name.js';
 export * from './check.js';
 export * from './check-base.js';
+export { getCandidate } from './get-candidate.js';
 export type * from './types.js';
