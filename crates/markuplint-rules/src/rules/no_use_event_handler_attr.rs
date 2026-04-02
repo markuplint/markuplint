c1ac2f7b6e5e32488a3833441066da787657264a
Yusuke Hirao <hirao@d-zero.co.jp>
2026-04-03 01:21:08 +0900

    fix(core): no-use-event-handler-attr: add Violation name field

--- crates/markuplint-rules/src/rules/no_use_event_handler_attr.rs
+++ crates/markuplint-rules/src/rules/no_use_event_handler_attr.rs
@@ -79,6 +79,7 @@ impl Rule for NoUseEventHandlerAttr {
 
                 violations.push(Violation {
                     rule_id: self.id().to_string(),
+                    name: None,
                     severity: rule_config.severity.clone(),
                     message: format!("The \"{}\" attribute is disallowed", html_attr.node_name),
                     line: html_attr.name.line,
